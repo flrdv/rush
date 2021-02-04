90181a872edf89d1017e93417f6799faead59d73
floordiv <pavloromanenko25@gmail.com>
2021-02-05 00:03:38 +0200

    proto.msgproto: simple protocol with fixed packet length (first 4 bytes of packet)

---
+++ proto/msgproto.py
@@ -0,0 +1,27 @@
+import socket
+from struct import pack, unpack
+
+
+FUTURE_MSG_LEN_BYTES = 4
+READ_CHUNK_SIZE = 4096
+
+
+def sendmsg(sock: socket.socket, data: bytes):
+    packet_len = pack('>I', len(data))
+
+    return sock.send(packet_len + data)
+
+
+def recvmsg(sock: socket.socket):
+    encoded_msglen = b''
+
+    while len(encoded_msglen) < FUTURE_MSG_LEN_BYTES:
+        encoded_msglen += sock.recv(FUTURE_MSG_LEN_BYTES - len(encoded_msglen))
+
+    msglen = sum(unpack('>I', encoded_msglen))
+    msg = b''
+
+    while len(msg) < msglen:
+        msg += sock.recv(READ_CHUNK_SIZE)
+
+    return msg
