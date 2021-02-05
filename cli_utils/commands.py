2b1d4f69ae46411692048df3d50c666b0efbce13
floordiv <pavloromanenko25@gmail.com>
2021-02-05 15:31:33 +0200

    cli_utils.commands: fixed import (removed rush. prefix)

--- cli_utils/commands.py
+++ cli_utils/commands.py
@@ -1,9 +1,9 @@
-import rush.servers.cluster_resolver.resolver
-from rush.cli_utils import run_as_daemon
+import servers.cluster_resolver.resolver
+from cli_utils import run_as_daemon
 
 
 def start_resolver(ip='localhost', port=11100):
-    resolver = rush.servers.cluster_resolver.resolver.Resolver(addr=(ip, port))
+    resolver = servers.cluster_resolver.resolver.Resolver(addr=(ip, port))
     run_as_daemon.run_function(resolver.run_blocking_handler)
 
 
