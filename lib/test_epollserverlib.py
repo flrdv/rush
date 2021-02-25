4acf93e3f1aacdb34d8dcff2c70a435436e72e21
floordiv <pavloromanenko25@gmail.com>
2021-02-25 21:22:59 +0200

    lib.test_epollserverlib: testing file to test epollserver lib

---
+++ lib/test_epollserverlib.py
@@ -0,0 +1,25 @@
+import epollserver
+
+epoll_server = epollserver.EpollServer(('localhost', 8801))
+
+
+@epoll_server.handler(on_event=epollserver.CONNECT)
+def handle_conn(_, server):
+    conn, addr = server.accept()
+    print('new conn from', addr)
+
+    return conn
+
+
+@epoll_server.handler(epollserver.RECEIVE)
+def handle_recvmsg(_, conn: epollserver.socket):
+    print(f'received data from {conn.getpeername()}: {conn.recv(10)}')
+    conn.send(b'hello!')
+
+
+@epoll_server.handler(epollserver.DISCONNECT)
+def handle_disconnect(_, conn):
+    print('disconnected:', conn.getpeername())
+
+
+epoll_server.start()
