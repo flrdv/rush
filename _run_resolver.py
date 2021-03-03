9cb54ff4b2bee2380a96a1171fdc7e59ed141151
floordiv <pavloromanenko25@gmail.com>
2021-03-03 10:59:07 +0200

    _run_resolver: changed default ip addr -> local addr of my rpi (no desktop linux available)

--- _run_resolver.py
+++ _run_resolver.py
@@ -2,5 +2,5 @@ import servers.resolver.resolver
 
 
 if __name__ == '__main__':
-    resolver = servers.resolver.resolver.Resolver()
+    resolver = servers.resolver.resolver.Resolver(('192.168.0.102', 11100))
     resolver.start()
