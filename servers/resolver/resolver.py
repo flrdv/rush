66ea5bfe1f708e73428f1add44b790a8fcbd55dc
floordiv <pavloromanenko25@gmail.com>
2021-03-04 15:01:55 +0200

    servers.resolver.resolver: logserver now is custom server. Added full support of custom servers requests, fixed bug when main server address wasn't removing

--- servers/resolver/resolver.py
+++ servers/resolver/resolver.py
@@ -12,7 +12,7 @@ DELETE_DATA = 2
 ADD_STATE = 3
 CLUSTER = 0
 MAINSERVER = 1
-LOGSERVER = 2
+CUSTOM_SERVER = 2
 RESPONSE_SUCC = 2
 RESPONSE_FAIL = 3
 STATE_DONE = 4
@@ -96,6 +96,37 @@ def delete_mainserver_addr(name):
         conn.commit()
 
 
+def add_custom_server(typeofserver, name, addr):
+    ip, port = addr
+
+    with sqlite3.connect(REGISTRY_DB) as conn:
+        cursor = conn.cursor()
+        cursor.execute('INSERT INTO servers VALUES (?, ?, ?, ?)',
+                       (typeofserver, name, ip, port))
+        conn.commit()
+
+
+def get_custom_server(typeofserver, name):
+    with sqlite3.connect(REGISTRY_DB) as conn:
+        cursor = conn.cursor()
+        cursor.execute('SELECT ip, port FROM servers WHERE typeofserver=? AND name=?',
+                       (typeofserver, name))
+
+        return cursor.fetchone() or (None, None)
+
+
+def delete_custom_server(typeofserver, name):
+    with sqlite3.connect(REGISTRY_DB) as conn:
+        cursor = conn.cursor()
+        query = 'DELETE FROM servers WHERE typeofserver=?'
+
+        if name != '*':
+            query += ' AND name=?'
+
+        cursor.execute(query, (typeofserver, name))
+        conn.commit()
+
+
 def response(code: int, text: bytes):
     return code.to_bytes(1, 'little') + text
 
@@ -117,23 +148,30 @@ def _handle_write_request(conn, request_to, body):
         add_mainserver_addr(name, ip, port)
         print(f'[RESOLVER] Added main server "{name}" with address {ip}:{port} '
               f'(by {client_ip}:{client_port})')
+    elif request_to == CUSTOM_SERVER:
+        typeofserver, name = parse_request_to_custom_server(name)
+
+        add_custom_server(typeofserver, name, (ip, port))
 
 
-def _handle_read_request(conn, request_to, body):
+def _handle_read_request(conn, request_to, name):
     client_ip, client_port = conn.getpeername()
 
     if request_to == CLUSTER:
-        print(f'[RESOLVER] Getting address for cluster "{body}"...', end=' ')
+        print(f'[RESOLVER] Getting address for cluster "{name}"...', end=' ')
         method = get_cluster_addr
     elif request_to == MAINSERVER:
-        print(f'[RESOLVER] Getting address for main server "{body}"...', end=' ')
+        print(f'[RESOLVER] Getting address for main server "{name}"...', end=' ')
         method = get_mainserver_addr
+    elif request_to == CUSTOM_SERVER:
+        typeofserver, name = parse_request_to_custom_server(name)
+        method = lambda nameofserver: get_custom_server(typeofserver, nameofserver)
     else:
         print('[RESOLVER] Received unknown REQUEST_TO code from '
               f'{client_ip}:{client_port} ({request_to})')
         return sendmsg(conn, response(RESPONSE_FAIL, b'bad-request-to'))
 
-    ip, port = method(body)
+    ip, port = method(name)
 
     if ip is None:
         print('fail (not found)')
@@ -145,24 +183,31 @@ def _handle_read_request(conn, request_to, body):
     sendmsg(conn, response(RESPONSE_SUCC, encoded_json))
 
 
-def _handle_delete_request(conn, request_to, body):
+def _handle_delete_request(conn, request_to, name):
     ip, port = conn.getpeername()
 
     if request_to == CLUSTER:
-        print(f'[RESOLVER] Deleting address of cluster "{body}"')
+        print(f'[RESOLVER] Deleting address of cluster "{name}"')
         method = delete_cluster_addr
     elif request_to == MAINSERVER:
-        print(f'[RESOLVER] Deleting address of main server "{body}"...', end=' ')
-        method = get_mainserver_addr
+        print(f'[RESOLVER] Deleting address of main server "{name}"...', end=' ')
+        method = delete_mainserver_addr
+    elif request_to == CUSTOM_SERVER:
+        typeofserver, name = parse_request_to_custom_server(name)
+        method = lambda nameofserver: delete_custom_server(typeofserver, nameofserver)
     else:
         print(f'[RESOLVER] Received unknown REQUEST_TO code from {ip}:{port} ({request_to})')
         return sendmsg(conn, response(RESPONSE_FAIL, b'bad-request-to'))
 
-    method(body)
+    method(name)
 
 
 def handle_bad_request_type(conn, *args):
     sendmsg(conn, response(RESPONSE_FAIL, b'bad-request-type'))
+
+
+def parse_request_to_custom_server(name):
+    return map(lambda item: item.strip(), name.split(':', maxsplit=1))
 
 
 class Resolver:
@@ -181,7 +226,7 @@ class Resolver:
             WRITE_DATA: _handle_write_request,
             READ_DATA: _handle_read_request,
             DELETE_DATA: _handle_delete_request,
-            ADD_STATE: ...
+            ADD_STATE: self._handle_add_state,
         }
 
     def start(self, threaded=False):
