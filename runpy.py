af92df95416bba76cbd7db2761fd971bf4db29f9
floordiv <pavloromanenko25@gmail.com>
2021-03-04 13:37:48 +0200

    runpy: removed useless tab in empty line xD

--- runpy.py
+++ runpy.py
@@ -5,6 +5,6 @@ import importlib
 if len(sys.argv) < 2:
     print('[USAGE] python3 runpy.py <path to file to run>')
     sys.exit(1)
-    
+
 path_to_module = sys.argv[1]
 importlib.import_module(path_to_module)
