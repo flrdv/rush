0f64153652052c26f1ab5d91589c14c512a6d11a
floordiv <pavloromanenko25@gmail.com>
2021-03-14 22:10:30 +0200

    see readme

--- _run_logserver.py
+++
@@ -1,6 +0,0 @@
-import servers.logserver.logserver as logserver
-
-
-if __name__ == '__main__':
-    my_logserver = logserver.LogServer(addr=('192.168.0.102', 11101))
-    my_logserver.start()
--- _run_resolver.py
+++
@@ -1,6 +0,0 @@
-import servers.resolver.resolver
-
-
-if __name__ == '__main__':
-    resolver = servers.resolver.resolver.Resolver(('192.168.0.102', 11100))
-    resolver.start()
