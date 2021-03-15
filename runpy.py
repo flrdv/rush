b50bd2b51bf04af5cf5279b934a3beaf9dad45bd
floordiv <pavloromanenko25@gmail.com>
2021-03-16 01:08:40 +0200

    runpy: now starting module as a child process

--- runpy.py
+++ runpy.py
@@ -1,10 +1,10 @@
 import sys
-import importlib
+from subprocess import Popen
 
 
 if len(sys.argv) < 2:
     print('[USAGE] python3 runpy.py <path to file to run>')
     sys.exit(1)
 
-path_to_module = sys.argv[1]
-importlib.import_module(path_to_module)
+process = Popen(['python3', sys.argv[1]])
+process.wait()
