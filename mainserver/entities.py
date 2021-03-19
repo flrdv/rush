7cad4ff922b0a9dad220fc8ed3b2e8b69a746f3a
floordiv <pavloromanenko25@gmail.com>
2021-03-19 20:20:05 +0200

    mainserver.entities: module that contains classes for mainserver.core abstractions. Curreently contains Filter (class that contains filter and returns bool when instance is being called with request), Handler (class that contains conn object, addr, ip, port and filter-entity attributes), HandlerInitializer (class that fully handles handlers initialization)

---
+++ mainserver/entities.py
@@ -0,0 +1,110 @@
+import re
+from json import loads, JSONDecodeError
+
+INITIAL_BYTESORDER = b'\x69\x04\x02\x00'
+
+
+def compare_filters(pattern: dict, source: dict):
+    source_items = source.items()
+
+    for key, value in pattern.items():
+        for source_key, source_value in source_items:
+            if re.fullmatch(key, source_key):
+                if source_value is None or re.fullmatch(value, source_value):
+                    break
+        else:
+            return False
+
+    return True
+
+
+class Filter:
+    """
+    Contains dict-sample and checks incoming request with dict-sample
+    """
+
+    def __init__(self, sample):
+        self.sample = sample
+
+    def __call__(self, request):
+        return compare_filters(self.sample, request)
+
+
+class Handler:
+    """
+    Contains handler filter (group), address, conn-object
+    """
+
+    def __init__(self, conn, filter_):
+        self.conn = conn
+        self.addr = conn.getpeername()
+        self.ip, self.port = self.addr
+        self.filter = filter_
+
+    def get_conn(self):
+        return self.conn
+
+    def get_filter(self):
+        return self.filter
+
+    def set_filter(self, new_filter):
+        self.filter = new_filter
+
+    def get_addr(self):
+        return self.addr
+
+
+class HandlerInitializer:
+    def __init__(self, conn):
+        self.conn = conn
+
+        self.current_step = 0
+
+    def next_step(self, core_server, msg):
+        if self.current_step > 2:
+            raise RuntimeError('handler has been already initialized')
+
+        response = self.steps[self.current_step](core_server, msg)
+        self.current_step += 1
+
+        return response
+
+    def step1(self, core_server, msg):
+        """
+        Step 1: receive b'\x69\x04\x02\x00' and send reversed back
+        """
+
+        if msg != b'\x69\x04\x02\x00':
+            return False
+
+        self.conn.send(INITIAL_BYTESORDER[::-1])
+
+        return True
+
+    def step2(self, core_server, msg):
+        """
+        Step 2: receive \x01 byte and send server's name
+        """
+
+        if msg != b'\x01':
+            return False
+
+        self.conn.send(core_server.name.encode())
+
+        return True
+
+    def step3(self, core_server, msg):
+        """
+        Step 3: receive handler's filter and response with
+        """
+
+        try:
+            return loads(msg)
+        except (ValueError, JSONDecodeError):
+            return False
+
+    steps = {
+        0: step1,
+        1: step2,
+        2: step3
+    }
