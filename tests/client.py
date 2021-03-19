3e18c3fc713d7954ff3b0909099ab21828f036fa
floordiv <pavloromanenko25@gmail.com>
2021-03-19 20:20:33 +0200

    tests.client: added logging, added 3 threads for testing (and receiving responses)

--- tests/client.py
+++ tests/client.py
@@ -1,13 +1,23 @@
 import socket
+from threading import Thread
 
-from lib.msgproto import sendmsg
+from lib.msgproto import sendmsg, recvmsg
 
 
-with socket.socket() as sock:
-    print('connecting...')
-    sock.connect(('192.168.0.102', 9090))
-    print('connected\nsending hello world...')
-    sendmsg(sock, b'hello, world!')
-    print('sent')
+def send(msg: bytes):
+    with socket.socket() as sock:
+        print('connecting...')
+        sock.connect(('192.168.0.102', 9090))
+        print('connected\nsending hello world...')
+        sendmsg(sock, msg)
+        print('sent\nreceiving response')
+        response = recvmsg(sock)
+        print('received:', response)
 
-print('closed connection')
+    print('closed connection')
+
+
+thread1 = Thread(target=send, args=(b'hello, world!',))
+thread2 = Thread(target=send, args=(b'not cool, but how?',))
+thread3 = Thread(target=send, args=(b'why is it? is it lorem ipsum? Yes!',))
+thread1.start(), thread2.start(), thread3.start()
