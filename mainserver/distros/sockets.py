cd5f238687af9dbf0dd963489ace41f93515e51c
floordiv <pavloromanenko25@gmail.com>
2021-03-20 02:52:36 +0200

    mainserver.distros.sockets: simple demo wrapper for sockets using lib.msgproto protocol

---
+++ mainserver/distros/sockets.py
@@ -0,0 +1,35 @@
+from lib import epollserver
+from mainserver.core import CoreServer
+from lib.msgproto import sendmsg, recvmsg
+
+
+def stringify_addr(addr):
+    return addr[0] + str(addr[1])
+
+
+class SimpleSocketServer:
+    def __init__(self):
+        self.clients = {}   # ip:port: conn
+
+        self.epollserver = epollserver.EpollServer(('0.0.0.0', 9090))
+
+        self.server_core = CoreServer(self.response)
+
+    def start(self):
+        self.server_core.start()
+
+    def response(self, response_to, response_body):
+        conn = self.clients[response_to]
+        sendmsg(conn, response_body)
+
+    def conn_handler(self, _, conn):
+        print('New connection:', stringify_addr(conn.getpeername()))
+
+    def disconn_handler(self, _, conn):
+        print('Disconnected:', stringify_addr(conn.getpeername()))
+
+    def requests_handler(self, _, conn):
+        msg = recvmsg(conn)
+        ip = stringify_addr(conn.getpeername())
+        print('New request from', ip, ':', msg)
+        self.server_core.send_update([ip, msg])
