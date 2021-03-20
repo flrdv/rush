97c4d659cefe35cf72df83d001bd3ee77ce80d34
floordiv <pavloromanenko25@gmail.com>
2021-03-20 23:55:18 +0200

    handler.sample: implemented handler.update_parser decorated function

--- handler/sample.py
+++ handler/sample.py
@@ -3,13 +3,18 @@ from typing import Dict
 from handler.client import Handler
 
 
-handler = Handler()
+handler = Handler(addr=('192.168.0.102', 10000))
+
+
+@handler.update_parser
+def parse_update(request_from, request):
+    return {'request-from': request_from, 'body': request}
 
 
 @handler()
 def handle(client, request: Dict[str, bytes]):
     print('New request:', request)
-    client.response(b'you said: ' + request['body'])
+    client.response(request['request-from'], b'you said: ' + request['body'])
 
 
 handler.start()
