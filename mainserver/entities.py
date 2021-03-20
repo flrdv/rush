7abf35e94afa0a028d9ce251b9680a9b2cc34d97
floordiv <pavloromanenko25@gmail.com>
2021-03-20 11:51:54 +0200

    mainserver.entities: now possible to set filter also by ip address (good feature that appeared from an error)

--- mainserver/entities.py
+++ mainserver/entities.py
@@ -6,7 +6,11 @@ from lib.msgproto import sendmsg
 INITIAL_BYTES_SEQUENCE = b'\x69\x04\x02\x00'
 
 
-def compare_filters(pattern: dict, source: dict):
+def compare_filters(pattern: dict or list, source: dict or list):
+    if isinstance(source, list) and isinstance(pattern, list):
+        return re.fullmatch(pattern[0], source[0]) and \
+               compare_filters(pattern[1], source[1])
+    
     source_items = source.items()
 
     for key, value in pattern.items():
@@ -29,7 +33,7 @@ class Filter:
         self.sample = sample
 
     def __call__(self, request: list):
-        return compare_filters(self.sample, request[1])
+        return compare_filters(self.sample, request)
 
 
 class Handler:
