e90db251ee16295ff237bbbd5d8b02c379229d52
floordiv <pavloromanenko25@gmail.com>
2021-03-21 02:38:41 +0200

    tests.client: written simple stress-test (that fucked up my rpi, lol)

--- tests/client.py
+++ tests/client.py
@@ -1,7 +1,92 @@
 import socket
-from threading import Thread
+from random import choice
+from time import time, sleep
+from threading import Thread, get_ident
 
 from lib.msgproto import sendmsg, recvmsg
+
+
+class StressTest:
+    def __init__(self, clients=200, packets_per_second=10,
+                 wait_response=True, test_time=10, requests_pool=None):
+        self.clients = clients
+        self.packets_per_second = packets_per_second
+        self.packets_maxtimeout = 1 / packets_per_second
+        self.wait_response = wait_response
+        self.test_time = test_time or 999999
+        self.threads = []
+
+        self.active = False
+        self.server_cant_handle_specified_load = 0
+        self.requests = requests_pool or [b"hello, world!",
+                                          b'lorem ipsum, my brother!',
+                                          b'it should be long request as fuck, but I\'m not sure']
+        self.iterations = {}    # thread-id: iters
+
+    def client(self):
+        ident = get_ident()
+        self.iterations[ident] = 0
+
+        with socket.socket() as sock:
+            sock.connect(('192.168.0.102', 9090))
+
+            began_at = time()
+
+            while time() <= began_at + self.test_time and self.active:
+                # began_iter = time()
+
+                for _ in range(self.packets_per_second):
+                    sendmsg(sock, choice(self.requests))
+
+                    if self.wait_response:
+                        started_waiting_at = time()
+
+                        try:
+                            recvmsg(sock, timeout=1)
+                        except TimeoutError:
+                            print('Server fucked up')
+                        else:
+                            if time() - started_waiting_at > self.packets_maxtimeout:
+                                self.server_cant_handle_specified_load += 1
+
+                # if time() - began_iter < 1:
+                #     self.finished_plan_faster_times += 1
+
+                self.iterations[ident] += 1
+
+        print(f'finished ({round(time() - began_at, 2)} secs)')
+
+    def start(self):
+        print('Starting', self.clients, 'clients...')
+        self.active = True
+
+        for _ in range(self.clients):
+            thread = Thread(target=self.client)
+            self.threads.append(thread)
+            thread.start()
+
+        print('Started. Time for stress-test estimated:', self.test_time)
+
+        try:
+            sleep(self.test_time)
+        except KeyboardInterrupt:
+            print('Aborting stress test before it\'s finished')
+
+        self.active = False
+
+        map(lambda thread_: thread_.join(), self.threads)
+
+        rps_for_every_client = [iters / self.packets_per_second
+                                for ident, iters in self.iterations.items()]
+        total_requests_sent = sum(rps_for_every_client)
+        total_rps = total_requests_sent / len(rps_for_every_client)
+
+        sleep(.5)
+
+        print('Total requests sent:', total_requests_sent)
+        print('RPS:', total_rps)
+        print('Server failed and didn\'t response with required max timeout between packets:',
+              self.server_cant_handle_specified_load)
 
 
 def send(msg: bytes):
@@ -9,16 +94,23 @@ def send(msg: bytes):
         sock.settimeout(1)
         print('connecting...')
         sock.connect(('192.168.0.102', 9090))
-        print('connected\nsending hello world...')
-        sendmsg(sock, msg)
-        print('sent\nreceiving response')
-        response = recvmsg(sock)
-        print('received:', response)
+        print('connected')
+        for _ in range(10):
+            print('sending:', msg)
+            sendmsg(sock, msg)
+            print('sent\nreceiving response')
+            response = recvmsg(sock)
+            print('received:', response)
 
     print('closed connection')
 
 
-thread1 = Thread(target=send, args=(b'hello, world!',))
-thread2 = Thread(target=send, args=(b'not cool, but how?',))
-thread3 = Thread(target=send, args=(b'why is it? is it lorem ipsum? Yes!',))
-thread1.start(), thread2.start(), thread3.start()
+# thread1 = Thread(target=send, args=(b'hello, world!',))
+# thread2 = Thread(target=send, args=(b'not cool, but how?',))
+# thread3 = Thread(target=send, args=(b'why is it? is it lorem ipsum? Yes!',))
+# thread1.start(), thread2.start(), thread3.start()
+
+if __name__ == '__main__':
+    stress_test = StressTest(clients=100,
+                             packets_per_second=10)
+    stress_test.start()
