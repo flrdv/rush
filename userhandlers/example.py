814ea9fab96b1412fc071806dbb990f4f5168126
floordiv <pavloromanenko25@gmail.com>
2021-04-02 01:07:51 +0300

    userhandlers.example: updated example user handler (responsing with HTTP 200 OK code)

--- userhandlers/example.py
+++ userhandlers/example.py
@@ -1,12 +1,13 @@
 import server as webserver
+from lib.entities import Request
 
 
 server = webserver.WebServer()
 
 
 @server.serve(func=lambda msg: True)
-def handler(request):
-    print('New request:', request)
+def handler(request: Request):
+    request.response(b'HTTP/1.1 200 OK\n\nHello, World!\n')
 
 
 server.start()
