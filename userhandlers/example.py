d5dfaf27a91b2add91282403d72408d273821c7e
floordiv <pavloromanenko25@gmail.com>
2021-04-03 22:27:08 +0300

    userhandles.example: updated example, using Response object now instead of raw bytes

--- userhandlers/example.py
+++ userhandlers/example.py
@@ -1,13 +1,15 @@
 import server as webserver
-from lib.entities import Request
+from lib.entities import Request, Response
 
 
 server = webserver.WebServer()
 
+default_response = Response('HTTP/1.1', 200, 'OK', 'Hello from rush!')
+
 
 @server.serve(func=lambda msg: True)
 def handler(request: Request):
-    request.response(b'HTTP/1.1 200 OK\n\nHello, World!\n')
+    request.response(default_response)
 
 
 server.start()
