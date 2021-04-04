c9f27f142a7bcba439bc4e4cdc80e31d95c75cfe
floordiv <pavloromanenko25@gmail.com>
2021-04-05 01:04:40 +0300

    userhandlers.example: added identical examples of routing handlers and filtered handlers, also added simple html markup for main page as a demo of working of webserver

--- userhandlers/example.py
+++ userhandlers/example.py
@@ -2,14 +2,36 @@ import server as webserver
 from lib.entities import Request, Response
 
 
-server = webserver.WebServer()
+server = webserver.WebServer(debug_mode=True)
 
-default_response = Response('HTTP/1.1', 200, 'OK', 'Hello from rush!')
+default_response = Response('HTTP/1.1', 200, 'You found an easter-egg!')
+wellcum_html_template = """
+<html>
+    <head>
+        <title>Welcome!</title>
+    </head>
+    <body>
+        <h1><tt>Welcome to Rush-webserver!</tt></h1>
+        <br><br>
+        <h3>If you see this page, Rush-webserver works correct</h3>
+    </body>
+</html>
+"""
 
 
-@server.serve(func=lambda msg: True)
-def handler(request: Request):
+@server.filter(func=lambda request: request.path == '/egg')
+def handler_with_filter(request: Request):
     request.response(default_response)
+
+
+@server.route('/easter')
+def routing_handler(request: Request):
+    request.response(default_response)
+
+
+@server.route('/')
+def mainpage_handler(request: Request):
+    request.response(Response(request.protocol, 200, wellcum_html_template))
 
 
 server.start()
