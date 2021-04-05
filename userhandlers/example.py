b31fa79f79abed90de1a3e24ebfe18510d6c84b8
floordiv <pavloromanenko25@gmail.com>
2021-04-05 03:18:55 +0300

    userhandlers.example: added all new features with new entities and files loading (with caching)

--- userhandlers/example.py
+++ userhandlers/example.py
@@ -1,37 +1,44 @@
 import server as webserver
-from lib.entities import Request, Response
+from utils.entities import Request, Response, Loader
 
-
-server = webserver.WebServer(debug_mode=True)
-
-default_response = Response('HTTP/1.1', 200, 'You found an easter-egg!')
-wellcum_html_template = """
-<html>
-    <head>
-        <title>Welcome!</title>
-    </head>
-    <body>
-        <h1><tt>Welcome to Rush-webserver!</tt></h1>
-        <br><br>
-        <h3>If you see this page, Rush-webserver works correct</h3>
-    </body>
-</html>
+"""
+This example is not just example: this is a preview of how
+webserver should be used. Like best usage practises
 """
 
 
-@server.filter(func=lambda request: request.path == '/egg')
-def handler_with_filter(request: Request):
-    request.response(default_response)
+server = webserver.WebServer(debug_mode=False)
+loader = Loader(caching=True)
+# shitcode just to avoid using loader inside webserver
+server.set_404_page(loader.load('404.html'))
+
+loader.cache_files(
+    '/index.html',
+    '/404.html',
+)
+
+
+"""
+These 2 handlers below are demonstrating 2 different ways
+of solving the same problem. Just a demo
+"""
+
+static_response = Response('HTTP/1.1', 200, 'You found an easter-egg!')
 
 
 @server.route('/easter')
 def routing_handler(request: Request):
-    request.response(default_response)
+    request.static_response(static_response)
 
 
-@server.route('/')
-def mainpage_handler(request: Request):
-    request.response(Response(request.protocol, 200, wellcum_html_template))
+@server.filter(func=lambda request: request.path == '/egg')
+def handler_with_filter(request: Request):
+    request.static_response(static_response)
+
+
+@server.filter(func=lambda request: True)
+def all_other_pages_handler(request: Request):
+    request.response(request.protocol, 200, loader.load(request.path, cache=False))
 
 
 server.start()
