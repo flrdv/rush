b6212bea4b2da0ed5dcb82e8d6d0b46c8f228be8
floordiv <pavloromanenko25@gmail.com>
2021-05-30 03:42:31 +0300

    core.server: added ability to response to clients. http responses rendering moved to core.utils.httputils, also added connect handlers and disconnect handlers

--- core/server.py
+++ core/server.py
@@ -4,10 +4,10 @@ proxied
 """
 
 from queue import Queue
+from select import EPOLLIN, EPOLLOUT
 from http_parser.http import HttpParser
 
-from lib.epollserver import EpollServer
-from utils.status_codes import status_codes
+from lib import epollserver
 
 DEFAULT_RECV_BLOCK_SIZE = 8192  # how many bytes are receiving per 1 socket read
 
@@ -22,49 +22,57 @@ class HttpServer:
                                                      parser: http_parser.http.HttpParser)
         """
 
-        self.epollserver = EpollServer(sock, maxconns=max_clients)
+        self.epollserver = epollserver.EpollServer(sock, maxconns=max_clients)
+        self.epollserver.add_handler(self._connect_handler, epollserver.CONNECT)
+        self.epollserver.add_handler(self._disconnect_handler, epollserver.DISCONNECT)
+        self.epollserver.add_handler(self._receive_handler, epollserver.RECEIVE)
+        self.epollserver.add_handler(self._response_handler, epollserver.RESPONSE)
 
         # I'm using http-parser but not parsing just to know when request has been ended
         # anyway I'm putting parser-object to the queue, so no new parsers entities
         # will be created
         self._requests_buff = {}    # conn: [parser, raw]
+        self._responses_buff = {}   # conn: b'...'
 
         self.requests = Queue()
 
     def _receive_handler(self, _, conn):
         parser, previously_received_body = self._requests_buff[conn]
         received_body_part = conn.recv(DEFAULT_RECV_BLOCK_SIZE)
-
         parser.execute(received_body_part, len(received_body_part))
-        current_body = previously_received_body + received_body_part
+
+        """
+        if message has been completely received, clear the buffer and send result to the queue
+        elif we received part of the body
+        """
 
         if parser.is_message_complete():
-            self.requests.put((current_body, conn, parser))
+            self.requests.put((previously_received_body + parser.recv_body(), conn, parser))
             self._requests_buff[conn] = [HttpParser(decompress=True), b'']
-        else:
-            self._requests_buff[conn][1] = current_body
+        elif parser.is_partial_body():
+            self._requests_buff[conn][1] += parser.recv_body()
 
+    def _response_handler(self, _, conn):
+        bytes_string = self._responses_buff[conn]
+        bytes_sent = conn.send(bytes_string)
+        new_bytes_string = bytes_string[:bytes_sent]
+        self._responses_buff[conn] = new_bytes_string
 
-def render_http_response(protocol, status_code, status_code_desc,
-                         content_type, headers, body):
-    # default headers
-    # TODO: add server-time header
-    final_headers = {
-        'Content-Length': len(body),
-        'Server': 'rush',
-        'Content-Type': content_type or 'text/html',
-        'Connection': 'keep-alive'
-    }
-    final_headers.update(headers or {})
-    body = body if isinstance(body, bytes) else body.encode()
+        if not new_bytes_string:
+            self.epollserver.direct_modify(conn, EPOLLIN)
 
-    # building time
-    status_description = status_code_desc or status_codes.get(status_code, 'NO DESCRIPTION')
+    def _connect_handler(self, _, conn):
+        # creating cell for conn once to avoid if-conditions in _receive_handler and
+        # _response_handler every time receiving new event on socket read/write
+        self._requests_buff[conn] = [HttpParser(decompress=True), b'']
+        self._responses_buff[conn] = b''
 
-    return b'%s %d %s\r\n%s\r\n\r\n%s' % (protocol.encode(), status_code,
-                                          status_description.encode(),
-                                          format_headers(final_headers), body)
+    def _disconnect_handler(self, _, conn):
+        self._requests_buff.pop(conn)
+        self._responses_buff.pop(conn)
 
+    def send(self, conn, data: bytes):
+        if not self._responses_buff[conn]:
+            self.epollserver.direct_modify(conn, EPOLLIN | EPOLLOUT)
 
-def format_headers(headers: dict):
-    return '\n'.join(f'{key}: {value}' for key, value in headers.items()).encode()
+        self._responses_buff[conn] += data
