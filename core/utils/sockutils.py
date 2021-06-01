5f67a15334e9e77cee338fe514952eb2a62e6985
floordiv <pavloromanenko25@gmail.com>
2021-06-01 23:32:07 +0300

    core.utils.sockutils: using built-in logging-capability logging format

--- core/utils/sockutils.py
+++ core/utils/sockutils.py
@@ -1,30 +1,25 @@
 from time import sleep
 
-from lib import simplelogger
 
-
-def bind_sock(logger: simplelogger.Logger, sock, addr, max_retries=99999,
+def bind_sock(logger, sock, addr, max_retries=99999,
               retries_timeout=3):
     addr_stringified = addr[0] + ':' + str(addr[1])
 
     for retry_num in range(1, max_retries + 1):
-        logger.write(f'[INIT] Trying to bind server on {addr_stringified}...',
-                     simplelogger.INFO)
+        logger.info(f'[INIT] Trying to bind server on {addr_stringified}...')
 
         try:
             sock.bind(addr)
-            logger.write(f'[INIT] Server successfully binded on {addr_stringified}',
-                         simplelogger.INFO)
+            logger.info(f'[INIT] Server successfully binded on {addr_stringified}')
 
             return True
         except OSError:
-            logger.write(f'[INIT] Failed to bind server on {addr_stringified} for {retry_num} ' +
-                         ('time' if retry_num == 1 else 'times'),
-                         simplelogger.ERROR)
+            logger.error(f'[INIT] Failed to bind server on {addr_stringified} for {retry_num} ' +
+                         ('time' if retry_num == 1 else 'times'))
 
         sleep(retries_timeout)
 
-    logger.write(f'[INIT] Failed to bind server on {addr_stringified}: max retries limit exceeded',
-                 simplelogger.ERROR)
+    logger.critical(f'[INIT] Failed to bind server on {addr_stringified}: '
+                    'max retries limit exceeded')
 
     return False
