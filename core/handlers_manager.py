e3a61222faafa12e03dbe138c0cdb46fc431929f
floordiv <pavloromanenko25@gmail.com>
2021-06-03 00:01:10 +0300

    core.handlers_manager: removed logging.baseConfig call (moved to webserver.py), removed requests_queue argument for process_worker() (taking requests queue directly from http server instance)

--- core/handlers_manager.py
+++ core/handlers_manager.py
@@ -6,8 +6,6 @@ from http_parser.http import HttpParser
 
 from core.entities import Handler, Request
 
-logging.basicConfig(filename='logs/handlers.log', level=logging.DEBUG,
-                    format='[%(asctime)s] [%(levelname)s] %(name)s: %(message)s')
 logger = logging.getLogger(__name__)
 
 
@@ -20,14 +18,15 @@ def err_handler_wrapper(err_handler_type, func, request):
         logger.exception(format_exc())
 
 
-def process_worker(http_server, handlers, err_handlers: dict,
-                   requests_queue: Queue):
+def process_worker(http_server, handlers, err_handlers: dict):
     """
     Function that infinitely running. Getting request from requests_queue and
     calling a handler that matches a request
     """
 
     request = Request(http_server)
+    # bind to avoid getting attribute of class instance every single request
+    requests_queue = http_server.requests
 
     while True:
         body, conn, parser = requests_queue.get()
