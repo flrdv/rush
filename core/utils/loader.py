668f94e38550592a48c0acd45fc3df8b7a32cd8e
floordiv <pavloromanenko25@gmail.com>
2021-06-04 01:37:00 +0300

    core.utils.loader: written simple loader that supports caching (using default AutoUpdatingCache constantly; planning to add an option of using user-defined cache implementions), added AutoUpdatingCache - cache that is updating files automatically after they has been modificated (using inotify; one more reason why this shit is only for linux)

--- core/utils/loader.py
+++ core/utils/loader.py
@@ -1,3 +1,12 @@
+import os.path
+import logging
+from threading import Thread
+
+import inotify.adapters
+from inotify.constants import IN_MODIFY
+
+logger = logging.getLogger(__name__)
+
 content_types = {
     'html': 'text',
     'css': 'text',
@@ -9,51 +18,61 @@ content_types = {
 
 
 class Loader:
-    def __init__(self, root='localfiles', caching=False):
-        self.root = root
-        self.caching = caching
+    def __init__(self, root='localfiles'):
+        self.root = os.path.join(root, '')
+        self.cache = AutoUpdatingCache(self.root)
+        self.cache.start()
 
-        self.cache = {}
-        self.paths_aliases = {}
-        self.default_404_response = """\
-<html>
-    <head>
-        <title>404 NOT FOUND</title>
-    </head>
-    <body>
-        <h6 align="center">404 REQUESTING PAGE NOT FOUND</h6>
-    </body>
-</html>        
-"""
+        self.cached_files = []
 
-    def load(self, path: str, load_otherwise: str or None = '/404.html',
-             cache: bool = None):
-        if path == '/':
-            path = '/index.html'
+    def load(self, filename, cache=True):
+        if self.root + filename in self.cache.cached_files:
+            return self.cache.get(filename)
 
-        if path in self.cache:
-            return self.cache[path]
-
-        try:
-            with open(self.root + path, 'rb') as fd:
-                content = fd.read()
-        except FileNotFoundError:
-            if not load_otherwise:
-                return self.default_404_response.encode(), 'text/html'
-
-            return self.load(load_otherwise, load_otherwise='', cache=cache)
-
-        if cache is None:
-            cache = self.caching
-
-        extension = path.split('.')[-1]
-        content_type = content_types.get(extension, 'text') + '/' + extension
+        with open(self.root + filename, 'rb') as fd:
+            content = fd.read()
 
         if cache:
-            self.cache[path] = (content, content_type)
+            self.cache.add(self.root + filename, content)
 
-        return content, content_type
+        return content
 
-    def cache_files(self, *files):
+    def cache(self, *files):
         for file in files:
-            self.load(file, cache=True)
+            with open(self.root + file, 'rb') as fd:
+                self.cache.add(self.root + file, fd.read())
+
+
+class AutoUpdatingCache:
+    """
+    Default cache that is using inotify lib to see when files has changed, to reload their content
+    """
+
+    def __init__(self, root):
+        self.root = root
+        self.inotify = inotify.adapters.Inotify()
+
+        self.cached_files = {}
+
+    def _events_listener(self):
+        for event in self.inotify.event_gen(yield_nones=False):
+            _, event_types, path, filename = event
+
+            if IN_MODIFY in event_types:
+                with open(path + '/' + filename, 'rb') as fd:
+                    self.cached_files[os.path.join(path, filename)] = fd.read()
+
+                logger.info(f'cache: updated file PATH={path} FILENAME={filename}')
+
+    def add(self, path_to_file, actual_content):
+        if path_to_file in self.cached_files:
+            return
+
+        self.cached_files[path_to_file] = actual_content
+        self.inotify.add_watch(path_to_file)
+
+    def get(self, path_to_file):
+        return self.cached_files[path_to_file]
+
+    def start(self):
+        Thread(target=self._events_listener).start()
