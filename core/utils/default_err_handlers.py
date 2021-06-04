6075996b162e198f204493683c7c5b03f8e81da5
floordiv <pavloromanenko25@gmail.com>
2021-06-05 01:31:40 +0300

    core.utils.default_err_handlers: implemented default error handlers. not-found err handler is trying to return <root>/404.html, otherwise returning default one, defined right in lib. internal-error err handler makes all the same, but trying to load <root>/500.html

--- core/utils/default_err_handlers.py
+++ core/utils/default_err_handlers.py
@@ -1,10 +1,39 @@
+from utils.exceptions import NotFound
+
+DEFAULT_404 = """\
+<html>
+    <head>
+        <title>404 NOT FOUND</title>
+    </head>
+    <body>
+        <br><br>
+        <h1 align="center"><tt>404 REQUESTING PAGE NOT FOUND</tt></h1>
+    </body>
+</html>
+"""
+DEFAULT_500 = """\
+<html>
+    <head>
+        <title>500 Internal Server Error</title>
+    </head>
+    <body>
+        <br><br>
+        <h1 align="center">500 Internal Server Error</h1>
+    </body>
+</html>
+
+"""
+
+
 def not_found(request):
-    """
-    TODO: add loader support to request object
-    """
+    try:
+        request.response_file('404.html')
+    except NotFound:
+        request.response(404, body=DEFAULT_404)
 
 
 def internal_error(request):
-    """
-    TODO: the same as not-found handler
-    """
+    try:
+        request.response_file('500.html')
+    except NotFound:
+        request.response(500, body=DEFAULT_500)
