531720b34ac1e30efac3b748bad916d0c6394404
floordiv <pavloromanenko25@gmail.com>
2021-06-05 15:43:25 +0300

    webserver: added logging switcher to turn logs off, fixed ImportError for importing core, core.utils, added redirects support, fixed exception that occurs after starting process with process_workers (forgot to pass all the arguments process worker needs)

--- webserver.py
+++ webserver.py
@@ -1,28 +1,35 @@
-import logging
-from typing import List, Dict
 from socket import socket
+import logging as _logging
 from psutil import cpu_count
+from typing import List, Dict
 from traceback import format_exc
 from multiprocessing import Process
 
-from core import server, process_workers as process_workers_lib, entities, utils
+import core.server
+import core.entities
+import core.utils.loader
+import core.process_workers
+import core.utils.termutils
+import core.utils.sockutils
+import core.utils.default_err_handlers
 
-if not utils.termutils.is_linux():
+if not core.utils.termutils.is_linux():
     raise RuntimeError('Rush-webserver is only for linux. Ave Maria!')
 
-logging.basicConfig(level=logging.DEBUG,  # noqa
+_logging.basicConfig(level=_logging.DEBUG,  # noqa
                     format='[%(asctime)s] [%(levelname)s] %(name)s: %(message)s',
                     handlers=[
-                        logging.FileHandler("logs/handlers.log"),
-                        logging.StreamHandler()]
+                        _logging.FileHandler("logs/handlers.log"),
+                        _logging.StreamHandler()]
                     )
-logger = logging.getLogger(__name__)
+logger = _logging.getLogger(__name__)
 
 
 class WebServer:
     def __init__(self, ip='localhost', port=8000, max_conns=None,
                  process_workers=None, loader_impl=None, cache_impl=None,
-                 sources_root='localfiles'):
+                 sources_root='localfiles', logging=True):
+        logger.disabled = logging
         self.addr = (ip, port)
 
         if process_workers is None:
@@ -33,32 +40,33 @@ class WebServer:
         self.process_workers_count = process_workers
 
         if max_conns is None:
-            max_conns = utils.termutils.get_max_descriptors()
+            max_conns = core.utils.termutils.get_max_descriptors()
 
         sock = socket()
-        utils.sockutils.bind_sock(logger, sock, self.addr)
+        core.utils.sockutils.bind_sock(sock, self.addr)
 
-        self.http_server = server.HttpServer(sock, max_conns)
+        self.http_server = core.server.HttpServer(sock, max_conns)
 
         self.handlers = []
         self.err_handlers = {
-            'not-found': utils.default_err_handlers.not_found,
-            'internal-error': utils.default_err_handlers.internal_error,
+            'not-found': core.utils.default_err_handlers.not_found,
+            'internal-error': core.utils.default_err_handlers.internal_error,
         }
         self.server_events_callbacks: Dict[callable or None] = {
             'on-startup': None,
             'on-shutdown': None,
         }
+        self.redirects = {}
 
         self.process_workers: List[Process] = []
-        self.loader = (loader_impl or utils.loader.Loader)(cache_impl, root=sources_root)
+        self.loader = (loader_impl or core.utils.loader.Loader)(cache_impl, root=sources_root)
 
     def route(self, path, methods=None, filter_=None):
         def decorator(func):
-            self.handlers.append(entities.Handler(func=func,
-                                                  filter_=filter_,
-                                                  path_route=path,
-                                                  methods=methods))
+            self.handlers.append(core.entities.Handler(func=func,
+                                                       filter_=filter_,
+                                                       path_route=path,
+                                                       methods=methods))
 
             return func
 
@@ -82,6 +90,12 @@ class WebServer:
 
         return func
 
+    def add_redirect(self, from_path, to):
+        self.redirects[from_path] = to
+
+    def add_redirects(self, redirects: dict):
+        self.redirects.update(redirects)
+
     def start(self):
         on_startup_event_callback = self.server_events_callbacks['on-startup']
 
@@ -92,9 +106,11 @@ class WebServer:
         logger.info(f'starting {self.process_workers_count} process workers')
 
         for _ in range(self.process_workers_count):
-            process = Process(target=process_workers_lib.process_worker, args=(self.http_server,
-                                                                               self.handlers,
-                                                                               self.err_handlers))
+            process = Process(target=core.process_workers.process_worker, args=(self.http_server,
+                                                                                self.loader,
+                                                                                self.handlers,
+                                                                                self.err_handlers,
+                                                                                self.redirects))
             self.process_workers.append(process)
             process.start()
             logger.debug(f'started process worker ident:{process.ident} with pid {process.pid}')
