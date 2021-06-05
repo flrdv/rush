6bdaf56d1010b49882e8a84c0a76fc658d2d4f3d
floordiv <pavloromanenko25@gmail.com>
2021-06-05 15:40:28 +0300

    core.utils.sockutils: updated log entries, now getting logger manually instead of passing it threw an argument

--- core/utils/sockutils.py
+++ core/utils/sockutils.py
@@ -1,25 +1,28 @@
+import logging
 from time import sleep
 
+logger = logging.getLogger(__name__)
 
-def bind_sock(logger, sock, addr, max_retries=99999,
+
+def bind_sock(sock, addr, max_retries=99999,
               retries_timeout=3):
     addr_stringified = addr[0] + ':' + str(addr[1])
 
     for retry_num in range(1, max_retries + 1):
-        logger.info(f'[INIT] Trying to bind server on {addr_stringified}...')
+        logger.info(f'trying to bind server on {addr_stringified}...')
 
         try:
             sock.bind(addr)
-            logger.info(f'[INIT] Server successfully binded on {addr_stringified}')
+            logger.info(f'server successfully binded on {addr_stringified}')
 
             return True
         except OSError:
-            logger.error(f'[INIT] Failed to bind server on {addr_stringified} for {retry_num} ' +
+            logger.error(f'failed to bind server on {addr_stringified} for {retry_num} ' +
                          ('time' if retry_num == 1 else 'times'))
 
         sleep(retries_timeout)
 
-    logger.critical(f'[INIT] Failed to bind server on {addr_stringified}: '
+    logger.critical(f'failed to bind server on {addr_stringified}: '
                     'max retries limit exceeded')
 
     return False
