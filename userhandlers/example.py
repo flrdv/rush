5db95cdd4367a2cc1a56f26e4cf77d1423c7a745
floordiv <pavloromanenko25@gmail.com>
2021-06-05 15:41:51 +0300

    userhandlers.example: added `server.start()`, using loader.cache_files instead of old loader.cache, getting logger with name "main", added redirecting (/easter -> /egg)

--- userhandlers/example.py
+++ userhandlers/example.py
@@ -2,14 +2,16 @@ import logging
 
 from webserver import WebServer
 
-logger = logging.getLogger(__name__)
+logger = logging.getLogger('main')
 server = WebServer(port=9090)
+
+server.add_redirect('/easter', '/egg')
 
 
 @server.on_startup
 def on_startup(loader):
     logger.info('Wow! I\'m on-startup event callback!')
-    loader.cache('index.html')
+    loader.cache_files('index.html')
 
 
 @server.on_shutdown
@@ -19,9 +21,13 @@ def on_shutdown():
 
 @server.route('/')
 def main_page_handler(request):
+    print('watcha!')
     request.response_file('index.html', headers={'Connection': 'close'})
 
 
 @server.route('*')
 def any_other_file_handler(request):
     request.response_file(request.path, headers={'Connection': 'close'})
+
+
+server.start()
