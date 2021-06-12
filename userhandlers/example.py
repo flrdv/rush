15e15097be404bda6b506cbe8d08b4896729c8d6
floordiv <pavloromanenko25@gmail.com>
2021-06-12 20:26:40 +0300

    userhandlers.example: removed headers Connection: close from everywhere cause it does not makes sense, just makes example webserver unusable

--- userhandlers/example.py
+++ userhandlers/example.py
@@ -21,18 +21,17 @@ def on_shutdown():
 
 @server.route('/')
 def main_page_handler(request):
-    print('watcha!')
-    request.response_file('index.html', headers={'Connection': 'close'})
+    request.response_file('index.html')
 
 
 @server.route('/egg')
 def egg_handler(request):
-    request.response_file('fuckoff.html', headers={'Connection': 'close'})
+    request.response_file('fuckoff.html')
 
 
 @server.route('*')
 def any_other_file_handler(request):
-    request.response_file(request.path, headers={'Connection': 'close'})
+    request.response_file(request.path)
 
 
 server.start()
