d0e578b1f154cf1e42947735d4abd23b153e285a
floordiv <pavloromanenko25@gmail.com>
2021-06-14 02:56:08 +0300

    userhandlers.example: added example of static response by path /hello

--- userhandlers/example.py
+++ userhandlers/example.py
@@ -29,6 +29,11 @@ def egg_handler(request):
     request.response_file('fuckoff.html')
 
 
+@server.route('/hello')
+def say_hello(request):
+    request.response(200, b'hello!')
+
+
 @server.route('*')
 def any_other_file_handler(request):
     request.response_file(request.path)
