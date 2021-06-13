cb3458353c0b0dae3d1e865355d50f665cc4dbae
floordiv <pavloromanenko25@gmail.com>
2021-06-14 00:34:52 +0300

    core.utils.sockutils: bind_sock(): added optional argument disable_logs. If true, it does not log anything. Made to avoid duplicating logs in forks

--- core/utils/sockutils.py
+++ core/utils/sockutils.py
@@ -5,8 +5,11 @@ logger = logging.getLogger(__name__)
 
 
 def bind_sock(sock, addr, max_retries=99999,
-              retries_timeout=3):
+              retries_timeout=3, disable_logs=True):
     addr_stringified = addr[0] + ':' + str(addr[1])
+
+    if disable_logs:
+        logger.disabled = True
 
     for retry_num in range(1, max_retries + 1):
         logger.info(f'trying to bind server on {addr_stringified}...')
@@ -14,6 +17,7 @@ def bind_sock(sock, addr, max_retries=99999,
         try:
             sock.bind(addr)
             logger.info(f'server successfully binded on {addr_stringified}')
+            logger.disabled = False
 
             return True
         except OSError:
@@ -24,5 +28,7 @@ def bind_sock(sock, addr, max_retries=99999,
 
     logger.critical(f'failed to bind server on {addr_stringified}: '
                     'max retries limit exceeded')
+
+    logger.disabled = False
 
     return False
