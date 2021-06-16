d0a2d4e8d3854f0fae9470b71dbb3145cdb82cfe
floordiv <pavloromanenko25@gmail.com>
2021-06-16 03:27:37 +0300

    core.termutils: added function set_max_descriptors(). Docs about it you can see in docstring of this function

--- core/utils/termutils.py
+++ core/utils/termutils.py
@@ -10,6 +10,26 @@ def get_max_descriptors():
         return None
 
 
+def set_max_descriptors(value=None):
+    """
+    Trying to set max descriptors. Always returns max descriptors value
+    """
+
+    # in case of security, only int or None is allowed
+    if value is not None and not isinstance(value, int):
+        raise ValueError('setting max descriptors: given value that is not int or None')
+
+    if value is None:
+        value = 'unlimited'
+
+    try:
+        check_output('ulimit -Sn ' + str(value))
+
+        return get_max_descriptors()
+    except ValueError:
+        return get_max_descriptors()
+
+
 def is_linux():
     return system() == 'Linux'
 
