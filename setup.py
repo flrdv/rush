0acf30e0294d1b59f2b2e81a69f9602921561b38
floordiv <pavloromanenko25@gmail.com>
2021-06-29 02:15:05 +0300

    setup: added flag `include_package_data`, without which one rush/defaultpages/ dir wasn't adding to the whl file

--- setup.py
+++ setup.py
@@ -23,6 +23,7 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/floordiv/rush",
     packages=find_packages(),
+    include_package_data=True,
     data_files=[('', defaultpages_content)],
     project_urls={
         "Bug Tracker": "https://github.com/floordiv/rush/issues",
