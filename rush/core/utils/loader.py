6d1efa86b668e93f0646f37157c011d5a562ed10
floordiv <pavloromanenko25@gmail.com>
2021-07-05 04:47:24 +0300

    rush.core.loader: deprecated root path in cache, now only loader works with root path, cache receives full path, optimized memory usage of caching responses with static files (now keeping only pre-rendered http headers, without body)

--- rush/core/utils/loader.py
+++ rush/core/utils/loader.py
@@ -11,58 +11,42 @@ from rush.core.utils import httputils
 
 logger = logging.getLogger(__name__)
 
-content_types = {
-    'html': 'text',
-    'css': 'text',
-    'png': 'image',
-    'ico': 'image',
-    'jpg': 'image',
-    'jpeg': 'image',
-}
-
 
 class Loader:
     def __init__(self, cache_impl, root):
-        self.root = root
-        self._cache = cache_impl(self.root)
+        self.root = os.path.join(root, '')
+        self._cache = cache_impl()
+
+        self.cache_response = lambda filename: self._cache.add_response(self.root + filename.lstrip('/'))
+        self.get_cached_response = lambda filename: self._cache.get_response(self.root + filename.lstrip('/'))
+
         self._cache.start()
 
         self.cached_files = []
-        self.cached_responses = {}
 
     def load(self, filename, cache=True):
-        if filename == '/':
-            filename = '/index.html'
-        elif filename[0] != '/':
-            filename = '/' + filename
+        file_path = self.root + filename.lstrip('/')
 
-        if filename in self._cache.cached_files:
-            return self._cache.get(filename)
+        if file_path in self._cache.cached_files:
+            return self._cache.get(file_path)
 
-        with open(self.root + filename, 'rb') as fd:
+        with open(file_path, 'rb') as fd:
             content = fd.read()
 
         if cache:
-            self._cache.add(filename, content)
+            self._cache.add(file_path, content)
 
         return content
 
     def cache_files(self, *files):
         for file in files:
-            if file[0] != '/':
-                file = '/' + file
+            file = file.lstrip('/')
 
             try:
                 with open(self.root + file, 'rb') as fd:
-                    self._cache.add(file, fd.read())
+                    self._cache.add(self.root + file, fd.read())
             except FileNotFoundError:
                 logger.error(f'trying to cache non-existing file: {self.root + file}')
-
-    def cache_response(self, filename):
-        return self._cache.add_response(filename, self.load(filename))
-
-    def get_cached_response(self, filename, otherwise=None):
-        return self._cache.get_response(filename, otherwise)
 
 
 class AutoUpdatingCache:
@@ -70,12 +54,13 @@ class AutoUpdatingCache:
     Default cache that is using inotify lib to see when files has changed, to reload their content
     """
 
-    def __init__(self, root):
-        self.root = root
+    def __init__(self):
         self.inotify = inotify.adapters.Inotify()
 
         self.cached_files = {}
-        self.cached_responses = {}
+        self.get = self.cached_files.get
+
+        self.cached_responses_headers = {}
 
     def _events_listener(self):
         for event in self.inotify.event_gen(yield_nones=False):
@@ -85,40 +70,42 @@ class AutoUpdatingCache:
                 file_path = os.path.join(path, filename)
 
                 with open(file_path, 'rb') as fd:
-                    new_content = fd.read()
-
-                    self.cached_files[file_path] = new_content
-                    self.add_response(file_path, new_content)
+                    self.cached_files[file_path] = fd.read()
 
                 logger.info(f'cache: updated file PATH={path} FILENAME={filename}')
 
     def add(self, path_to_file, actual_content):
-        if path_to_file[0] != '/':
-            path_to_file = '/' + path_to_file
-
         self.cached_files[path_to_file] = actual_content
 
         try:
-            self.inotify.add_watch(self.root + path_to_file)
+            self.inotify.add_watch(path_to_file)
         except InotifyError as exc:
-            logger.error(f'failed to start watching file {self.root + path_to_file}: {exc}'
+            logger.error(f'failed to start watching file {path_to_file}: {exc}'
                          f'\nFull traceback:\n{format_exc()}')
 
-    def get(self, path_to_file):
-        return self.cached_files[path_to_file]
+    """
+    Here was get() method. I replaced it with lookup in __init__ to avoid useless
+    methods proxying
+    """
 
-    def add_response(self, filename, new_content):
-        if new_content is None:
-            new_content = self.get(filename)
+    def add_response(self, filename):
+        if filename not in self.cached_files:
+            with open(filename, 'rb') as fd:
+                self.add(filename, fd.read())
 
-        response = httputils.render_http_response((1, 1), 200, 'OK',
-                                                  None, new_content)
-        self.cached_responses[filename] = response
+        response_file = self.cached_files[filename]
+        response_headers = httputils.render_http_response((1, 1), 200, 'OK',
+                                                          {'Content-Length': len(response_file)}, b'')
+        self.cached_responses_headers[filename] = response_headers
 
-        return response
+        return response_headers + response_file
 
-    def get_response(self, filename, otherwise):
-        return self.cached_responses.get(filename, otherwise)
+    def get_response(self, filename) -> bytes or None:
+        if filename not in self.cached_responses_headers or \
+                filename not in self.cached_files:
+            return None
+
+        return self.cached_responses_headers[filename] + self.cached_files[filename]
 
     def start(self):
         Thread(target=self._events_listener).start()
