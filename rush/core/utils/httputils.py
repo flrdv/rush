01998b922db0e967596b9cd35c4a19ad2f67ab3c
floordiv <pavloromanenko25@gmail.com>
2021-07-10 16:41:48 +0300

    rush.core.utils.httputils: removed urllib parameters string parser - written my own implementation, that is faster for ~70%, added url decoding function (from urllib, but edited by me: faster for ~10-20%)

--- rush/core/utils/httputils.py
+++ rush/core/utils/httputils.py
@@ -16,6 +16,9 @@ def render_http_response(protocol, status_code, status_code_desc,
     status_description = status_code_desc or status_codes.get(status_code, 'NO DESCRIPTION')
     protocol = f'HTTP/{".".join(protocol)}'.encode()
 
+    # TODO: in Python 3.11, they promised to make C-style formatting
+    #       as fast as f-strings, but only if string is simple (%s, %r, %a).
+    #       so don't forget to replace %d with %s (cast status_code to string)
     return b'%s %d %s\r\n%s\r\n\r\n%s' % (protocol, status_code,
                                           status_description.encode(),
                                           '\r\n'.join(f'{key}: {value}' for key, value in headers.items()).encode(),
@@ -27,121 +30,40 @@ def format_headers(headers: dict):
     return '\n'.join(f'{key}: {value}' for key, value in headers.items()).encode()
 
 
-"""
-There will be a part from urllib.parse with some changes
-"""
+def parse_params(params):
+    """
+    Returns dict with params (empty if no params given)
+    """
+
+    pairs = {}
+
+    for attr in params.split(b'&' if b'&' in params else b';'):
+        key, value = attr.decode().split('=')
+
+        if key not in pairs:
+            pairs[key] = [value]
+        else:
+            pairs[key].append(value)
+
+    return pairs
+
 
 _hexdig = '0123456789ABCDEFabcdef'
-_hextobyte = None
+_hextobyte = {(a + b).encode(): bytes.fromhex(a + b)
+              for a in _hexdig for b in _hexdig}
 
 
-def _noop(obj):
-    return obj
+def decode_url(bytestring):
+    if b'%' not in bytestring:
+        return bytestring
 
+    bits = bytestring.split(b'%')
+    decoded: bytes = bits[0]
 
-def _decode_args(args, encoding='ascii',
-                 errors='strict'):
-    return tuple(x.decode(encoding, errors) if x else '' for x in args)
-
-
-def _encode_result(obj, encoding='ascii',
-                   errors='strict'):
-    return obj.encode(encoding, errors)
-
-
-def _coerce_args(*args):
-    # Invokes decode if necessary to create str args
-    # and returns the coerced inputs along with
-    # an appropriate result coercion function
-    #   - noop for str inputs
-    #   - encoding function otherwise
-    if isinstance(args[0], str):
-        return args + (_noop,)
-    return _decode_args(args) + (_encode_result,)
-
-
-def parse_qs(qs, keep_blank_values=False, strict_parsing=False,
-             encoding='utf-8', errors='replace', max_num_fields=None, separator='&'):
-    return dict(parse_qsl(qs, keep_blank_values, strict_parsing,
-                          encoding=encoding, errors=errors,
-                          max_num_fields=max_num_fields, separator=separator))
-
-
-def unquote_to_bytes(string):
-    """unquote_to_bytes('abc%20def') -> b'abc def'."""
-    # Note: strings are encoded as UTF-8. This is only an issue if it contains
-    # unescaped non-ASCII characters, which URIs should not.
-    if not string:
-        return b''
-    if isinstance(string, str):
-        string = string.encode('utf-8')
-    bits = string.split(b'%')
-    if len(bits) == 1:
-        return string
-    res = [bits[0]]
-    append = res.append
-    # Delay the initialization of the table to not waste memory
-    # if the function is never called
-    global _hextobyte
-    if _hextobyte is None:
-        _hextobyte = {(a + b).encode(): bytes.fromhex(a + b)
-                      for a in _hexdig for b in _hexdig}
     for item in bits[1:]:
         try:
-            append(_hextobyte[item[:2]])
-            append(item[2:])
+            decoded += _hextobyte[item[:2]] + item[2:]
         except KeyError:
-            append(b'%')
-            append(item)
-    return b''.join(res)
+            decoded += b'%' + item
 
-
-_asciire = re.compile('([\x00-\x7f]+)')
-
-
-def unquote(string, encoding='utf-8', errors='replace'):
-    if isinstance(string, bytes):
-        return unquote_to_bytes(string).decode(encoding, errors)
-    if '%' not in string:
-        return string
-    bits = _asciire.split(string)
-    res = [bits[0]]
-    append = res.append
-    for i in range(1, len(bits), 2):
-        append(unquote_to_bytes(bits[i]).decode(encoding, errors))
-        append(bits[i + 1])
-    return ''.join(res)
-
-
-def parse_qsl(qs, keep_blank_values=False, strict_parsing=False,
-              encoding='utf-8', errors='replace', max_num_fields=None, separator='&'):
-    qs, _coerce_result = _coerce_args(qs)
-    separator, _ = _coerce_args(separator)
-    r = []
-    for name_value in qs.split(separator):
-        if not name_value and not strict_parsing:
-            continue
-        nv = name_value.split('=', 1)
-        if len(nv) != 2:
-            if strict_parsing:
-                raise ValueError("bad query field: %r" % (name_value,))
-            # Handle case of a control-name with no equal sign
-            if keep_blank_values:
-                nv.append('')
-            else:
-                continue
-        if len(nv[1]) or keep_blank_values:
-            name = nv[0].replace('+', ' ')
-            name = unquote(name, encoding=encoding, errors=errors)
-            name = _coerce_result(name)
-            value = nv[1].replace('+', ' ')
-            value = unquote(value, encoding=encoding, errors=errors)
-            value = _coerce_result(value)
-            r.append((name, value))
-    return r
-
-
-"""
-Some part from urllib.parse has been ended
-"""
-
+    return decoded
