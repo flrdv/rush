2162e5aa41e80d3663a9412843773c68e52b85aa
floordiv <pavloromanenko25@gmail.com>
2021-07-14 00:53:26 +0300

    renaming: rush.core.utils.httputils -> rush.utils.httputils. Also added rendering not only http response, but http request

--- rush/utils/httputils.py
+++ rush/utils/httputils.py
@@ -1,10 +1,14 @@
-import re
-
 from rush.utils.status_codes import status_codes
 
 
-def render_http_response(protocol, status_code, status_code_desc,
-                         user_headers, body):
+def format_headers(headers: dict):
+    return '\n'.join(f'{key}: {value}'
+                     for key, value in headers.items())\
+               .encode()
+
+
+def render_http_response(protocol: tuple, code: int, status_code: str or None,
+                         user_headers: dict, body: str or bytes):
     # default headers
     # TODO: add server-time header
     headers = {
@@ -13,21 +17,33 @@ def render_http_response(protocol, status_code, status_code_desc,
         'Connection': 'keep-alive',
         **(user_headers or {})
     }
-    status_description = status_code_desc or status_codes.get(status_code, 'NO DESCRIPTION')
+    status_description = status_code or status_codes.get(code, 'NO STATUS CODE')
     protocol = f'HTTP/{".".join(protocol)}'.encode()
 
     # TODO: in Python 3.11, they promised to make C-style formatting
     #       as fast as f-strings, but only if string is simple (%s, %r, %a).
     #       so don't forget to replace %d with %s (cast status_code to string)
-    return b'%s %d %s\r\n%s\r\n\r\n%s' % (protocol, status_code,
-                                          status_description.encode(),
-                                          '\r\n'.join(f'{key}: {value}' for key, value in headers.items()).encode(),
+
+    # I'm not using format_headers() function here just to avoid useless calling
+    # as everybody knows, functions' calls are a bit expensive in CPython
+    return b'%s %d %s\r\n%s\r\n\r\n%s' % (protocol, code, status_description.encode(),
+                                          '\r\n'.join(f'{key.lower()}: {value}'
+                                                      for key, value in headers.items())
+                                                .encode(),
                                           body if isinstance(body, bytes) else body.encode())
 
 
-# just if somebody will need this
-def format_headers(headers: dict):
-    return '\n'.join(f'{key}: {value}' for key, value in headers.items()).encode()
+def render_http_request(method: bytes, path: str, protocol: tuple,
+                        headers: dict, body: bytes):
+    if b'content-length' not in headers:
+        headers[b'content-length'] = len(body)
+
+    protocol = f'HTTP/{".".join(protocol)}'.encode()
+
+    return b'%s %s %s\r\n%s\r\n\r\n%s' % (method, path.encode(), protocol,
+                                          '\r\n'.join(f'{key.decode().lower()}: {value.decode()}'
+                                                      for key, value in headers.items()).encode(),
+                                          body)
 
 
 def parse_params(params):
