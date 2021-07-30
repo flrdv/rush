20175da947fcacb9bd5363af0749088574150c52
floordiv <pavloromanenko25@gmail.com>
2021-07-31 02:11:15 +0300

    rush.core.httpserver: using `CaseInsensetiveDict` by default for headers (clearing and re-using object again and again), added detecting of receiving chunked request/plain file, delivering them to user by callbacks; if parser error occurs while parsing request from user, user is being disconnected automatically; added more look-ups

--- rush/core/httpserver.py
+++ rush/core/httpserver.py
@@ -8,10 +8,11 @@ import logging
 from socket import MSG_PEEK
 from traceback import format_exc
 from httptools import HttpRequestParser
-from httptools.parser.errors import HttpParserError
 from select import epoll, EPOLLIN, EPOLLOUT, EPOLLHUP
+from httptools.parser.errors import HttpParserError, HttpParserCallbackError
 
-from rush.core.utils.httputils import decode_url
+from rush.utils.httputils import decode_url
+from rush.core.entities import CaseInsensitiveDict
 
 logger = logging.getLogger(__name__)
 
@@ -20,17 +21,28 @@ DEFAULT_RECV_BLOCK_SIZE = 8192  # how many bytes are receiving per 1 socket read
 
 
 class Protocol:
-    def __init__(self, call_handler, conn):
+    """
+    Initializing headers dictionary in the beginning, but individual for every
+    object
+    """
+    headers = CaseInsensitiveDict()
+
+    def __init__(self, call_handler, conn, send_http_response: callable):
         self.call_handler = call_handler
+        self.send_http_response = send_http_response
         self.parser = None  # will be set later
         self.conn = conn
         self.path = None
-        self.parameters = ''
+        self.parameters = b''
         self.fragment = None
-        self.headers = {}
         self.body = b''
+        # self.chunked = False
+        self.file = False
 
         self.received = False
+
+        self._on_chunk = None
+        self._on_complete = None
 
     def on_url(self, url):
         url = decode_url(url)
@@ -45,18 +57,50 @@ class Protocol:
 
         self.path = url
 
-    def on_header(self, name, value):
-        self.headers[name] = value
+    def on_header(self, name: bytes, value: bytes):
+        self.headers[name.decode()] = value.decode()
 
-    def on_body(self, body):
-        self.body += body
+    def on_headers_complete(self):
+        if self.headers.get('transfer-encoding') == 'chunked' or \
+                self.headers.get('content-type', '').startswith('multipart/'):
+            self.file = True
+            request_obj = self._call_handler()
+
+            if request_obj is None:
+                # just got redirected a client
+                return
+
+            # so long ang ugly only cause variables unpacking takes
+            # only 1 instruction for PVM
+            self._on_chunk, self._on_complete = request_obj.on_chunk, request_obj.on_complete
+
+    def on_body(self, body: bytes):
+        if self.file:
+            self._on_chunk(body)
+        else:
+            self.body += body
 
     def on_message_complete(self):
-        http_version = self.parser.get_http_version()
-        self.call_handler(self.body, self.conn, (http_version[0], http_version[2]),
-                          self.parser.get_method(), self.path, self.parameters,
-                          self.fragment, self.headers)
         self.received = True
+
+        if self.file:
+            self._on_complete()
+        else:
+            self._call_handler()
+
+    def _call_handler(self):
+        http_version = self.parser.get_http_version()
+
+        return self.call_handler(self.send_http_response,
+                                 self.body,
+                                 self.conn,
+                                 (http_version[0], http_version[2]),
+                                 self.parser.get_method(),
+                                 self.path,
+                                 self.parameters,
+                                 self.fragment,
+                                 self.headers,
+                                 self.file)
 
 
 class HttpServer:
@@ -65,7 +109,7 @@ class HttpServer:
         self.sock = sock
         self.max_conns = max_conns
         self.epoll = None
-        self._conns = {}    # fileno: conn
+        self._conns = {}  # fileno: conn
 
         # every client has only one entity of parser and protocol
         # on each request, entities are just re-initializing
@@ -77,14 +121,23 @@ class HttpServer:
 
         try:
             parser.feed_data(conn.recv(DEFAULT_RECV_BLOCK_SIZE))
+        except HttpParserCallbackError as exc:
+            logger.error(f'an error occurred in callback: {exc}')
+            logger.exception(f'full error trace:\n{format_exc()}')
+
+            return self._call_handler(self._disconnect_handler,
+                                      self._conns.pop(conn.fileno()))
         except HttpParserError as exc:
             # if some error occurred, relationship won't be good in future
             logger.error(f'disconnected user due to parsing request error: {exc}')
+
             return self._call_handler(self._disconnect_handler,
                                       self._conns.pop(conn.fileno()))
 
         if protocol.received:
-            protocol.__init__(self.on_message_complete, conn)
+            protocol.__init__(self.on_message_complete,
+                              conn,
+                              self.send)
             parser.__init__(protocol)
             protocol.parser = parser
             self._requests_buff[conn] = (parser, protocol)
@@ -101,7 +154,9 @@ class HttpServer:
     def _connect_handler(self, conn):
         # creating cell for conn once to avoid if-conditions in _receive_handler and
         # _response_handler every time receiving new event on socket read/write
-        protocol_instance = Protocol(self.on_message_complete, conn)
+        protocol_instance = Protocol(self.on_message_complete,
+                                     conn,
+                                     self.send)
         new_parser = HttpRequestParser(protocol_instance)
         protocol_instance.parser = new_parser
         self._requests_buff[conn] = (new_parser, protocol_instance)
@@ -112,12 +167,14 @@ class HttpServer:
         self._responses_buff.pop(conn)
         conn.close()
 
-    def _call_handler(self, handler, conn):
+    @staticmethod
+    def _call_handler(handler, conn):
         try:
             handler(conn)
         except Exception as exc:
-            logger.exception('Caught an unhandled exception in handler '
-                             f'"{handler.__name__}":\n{format_exc()}')
+            logger.error('Caught an unhandled exception in handler '
+                         f'"{handler.__name__}": {exc}')
+            logger.exception(f'detailed error trace:\n{format_exc()}')
 
     def send(self, conn, data: bytes):
         if not self._responses_buff[conn]:
@@ -143,11 +200,13 @@ class HttpServer:
         connect = self._connect_handler
         receive = self._receive_handler
         response = self._response_handler
+        poll = polling.poll
+        accept = sock.accept
 
         while True:
-            for fileno, event in polling.poll(1):
+            for fileno, event in poll(1):
                 if fileno == serversock_fileno:
-                    conn, addr = sock.accept()
+                    conn, addr = accept()
                     call_handler(connect, conn)
                     conn.setblocking(False)
                     polling.register(conn, EPOLLIN)
