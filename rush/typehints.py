ff5f20c83c06833b50b323651ca44d1de1d4550e
floordiv <pavloromanenko25@gmail.com>
2021-10-19 01:31:29 +0300

    rush.typehints: added typehints HTTPMethods (set), Nothing (NewType, refers to None), RoutePath (string or bytes) and Coroutine (Callable that receives Any as arguments and is Awaitable)

--- rush/typehints.py
+++ rush/typehints.py
@@ -1,12 +1,16 @@
 import socket
-from typing import Callable, BinaryIO
+from typing import Callable, Any, Awaitable, BinaryIO, Union, NewType
 
+Coroutine = Callable[[Any], Awaitable]
 Path = str
+RoutePath = Union[str, bytes]
 FileDescriptor = BinaryIO
 URI = bytes
 URIParameters = bytes
 URIFragment = bytes
 HTTPMethod = bytes
+HTTPMethods = set
 HTTPVersion = str
 Connection = socket.socket
 HttpResponseCallback = Callable
+Nothing = NewType('Nothing', None)
