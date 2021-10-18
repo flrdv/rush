84fa0efe394092a11f3eb81ad9bfb417713a4195
floordiv <pavloromanenko25@gmail.com>
2021-10-19 00:31:10 +0300

    rush.dispatcher.default: written default dispatcher that behaves both like decorators from flask and registering routes as in aiohttp; written class Handler, similar to it's analogue from Rush v2; written class Route that works pretty similar to it's brother from aiohttp

--- rush/dispatcher/default.py
+++ rush/dispatcher/default.py
@@ -1,2 +1,146 @@
-class CallbackDispatcher:
-    ...
+from asyncio import iscoroutine
+from typing import (Dict, Any, Callable, Awaitable, Union, Iterable)
+
+import exceptions
+from entities import Dispatcher, Request
+from utils.httpmethods import ALL_METHODS
+from typehints import RoutePath, Coroutine
+from utils.stringutils import make_sure_bytes_or_none
+
+
+class Handler:
+    """
+    A class that describes handler. Keeps it routing path,
+    methods, etc.
+    """
+
+    def __init__(self,
+                 handler: Callable[[Request], Awaitable],
+                 path: RoutePath,
+                 methods: Iterable[Any[ALL_METHODS]],
+                 any_path: bool
+                 ):
+        self.handler = handler
+        self.path = path
+        self.methods = methods
+        self.any_path = any_path
+
+
+class Route:
+    """
+    Mainly class for cases when you need to add routes without using
+    decorators, but using dp.add_routes([...])
+    """
+
+    def __init__(self,
+                 handler: Coroutine,
+                 path: RoutePath,
+                 method_or_methods: Union[str, bytes, Iterable] = ALL_METHODS
+                 ):
+        self.handler = handler
+        self.path = path
+
+        if not isinstance(method_or_methods, Iterable):
+            method_or_methods = {method_or_methods}
+
+        self.methods = method_or_methods
+
+
+class SimpleAsyncDispatcher(Dispatcher):
+    def __init__(self):
+        self.usual_handlers: Dict[bytes, Handler] = {}
+        self.any_paths_handlers: Dict[Any[ALL_METHODS], Handler] = {
+            method: None for method in ALL_METHODS
+        }
+
+    async def process_request(self,
+                              request: Request
+                              ) -> None:
+        if request.path not in self.usual_handlers:
+            handler = self.any_paths_handlers[request.method]
+
+            if handler is None:
+                raise exceptions.HTTPNotFound(request)
+        else:
+            handler = self.usual_handlers[request.path]
+
+        await handler.handler(request)
+
+    def route(self,
+              path: RoutePath,
+              method: Union[str, bytes, None] = None,
+              methods: Union[Iterable[Any[ALL_METHODS]]] = ALL_METHODS
+              ):
+
+        if method is not None:
+            methods = {make_sure_bytes_or_none(method.upper())}
+
+        def deco(coro: Callable[[Request], Awaitable]):
+            if not methods:
+                raise exceptions.NoMethodsProvided(str(coro))
+
+            if not iscoroutine(coro):
+                raise exceptions.HandlerMustBeCoroutineError(str(coro))
+
+            self._put_handler(Handler(
+                handler=coro,
+                path=make_sure_bytes_or_none(path),
+                methods=set(methods),
+                any_path=path is None
+            ))
+
+            return coro
+
+        return deco
+
+    def get(self,
+            path: RoutePath):
+        return self.route(path, 'GET')
+
+    def post(self, path: RoutePath):
+        return self.route(path, 'POST')
+
+    def head(self, path: RoutePath):
+        return self.route(path, 'HEAD')
+
+    def put(self, path: RoutePath):
+        return self.route(path, 'PUT')
+
+    def trace(self, path: RoutePath):
+        return self.route(path, 'TRACE')
+
+    def connect(self, path: RoutePath):
+        return self.route(path, 'CONNECT')
+
+    def delete(self, path: RoutePath):
+        return self.route(path, 'DELETE')
+
+    def options(self, path: RoutePath):
+        return self.route(path, 'OPTIONS')
+
+    def patch(self, path: RoutePath):
+        return self.route(path, 'PATCH')
+
+    def add_routes(self, routes: Iterable[Route]):
+        for route in routes:
+            self.add_route(route)
+
+    def add_route(self, route: Route):
+        self._put_handler(Handler(
+            handler=route.handler,
+            path=route.path,
+            methods=route.methods,
+            any_path=route.path is None
+        ))
+
+    def _put_handler(self, handler: Handler) -> None:
+        if handler.any_path:
+            self._add_any_path_handler(handler)
+        else:
+            self.usual_handlers[handler.path] = handler
+
+    def _add_any_path_handler(self, handler: Handler) -> None:
+        assert handler.any_path, TypeError('expected any path handler, got usual handler instead')
+
+        for method in handler.methods:
+            self.any_paths_handlers[method] = handler
