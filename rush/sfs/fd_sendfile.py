f77ca33d2e4e2b2e53a80982640a572b59177e26
floordiv <pavloromanenko25@gmail.com>
2021-10-19 01:29:45 +0300

    renaming: rush.sfs -> rush.sfs.fd_sendfile

--- rush/sfs/fd_sendfile.py
+++ rush/sfs/fd_sendfile.py
@@ -6,33 +6,8 @@ as for anything else, including local server's filesystem watching
 
 from typing import Dict, Union
 
-from .typehints import Path, Connection, HttpResponseCallback, FileDescriptor
-
-
-class SFS:
-    def add_file(self, path: Path) -> None:
-        """
-        Add file by it's path
-        """
-
-    def remove_file(self, path: Path) -> None:
-        """
-        Remove file by it's path
-        """
-
-    def send_file(self,
-                  path: Path,
-                  conn: Connection,
-                  http_response: HttpResponseCallback,
-                  code: int,
-                  status: Union[str, bytes, None],
-                  headers: Union[Dict, None]
-                  ) -> None:
-        """
-        Send a http response all by cache.
-
-
-        """
+from sfs.base import SFS
+from typehints import Path, Connection, HttpResponseCallback, FileDescriptor
 
 
 class SimpleDevSFS(SFS):
@@ -58,3 +33,4 @@ class SimpleDevSFS(SFS):
                   headers: Union[Dict, None]
                   ) -> None:
         ...
+
