9f6f306460aaa458d6b6547e0e4577e1d0ad8749
floordiv <pavloromanenko25@gmail.com>
2021-10-18 21:02:11 +0300

    rush.typehints: added typehints for uri parameters and fragment

--- rush/typehints.py
+++ rush/typehints.py
@@ -4,6 +4,8 @@ from typing import Callable, BinaryIO
 Path = str
 FileDescriptor = BinaryIO
 URI = bytes
+URIParameters = bytes
+URIFragment = bytes
 HTTPMethod = bytes
 HTTPVersion = str
 Connection = socket.socket
