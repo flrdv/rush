b03fd580d591f189ab7bfc84b85194c8123ddba5
floordiv <pavloromanenko25@gmail.com>
2021-10-24 01:36:02 +0300

    rush.try_asyncio_server: hardcoded for hello world http server, written on `loop.create_server`, so it's absolutely async; has handlers and processes forking

--- rush/try_asyncio_server.py
+++ rush/try_asyncio_server.py
@@ -1,20 +1,28 @@
 import uvloop
 import asyncio
+from functools import partial
 from typing import Optional
 from uvloop.loop import TCPTransport
 from multiprocessing import Process, cpu_count
 
 
-class HardcodedHTTPResponse(asyncio.Protocol):
-    def __init__(self, *args, **kwargs):
+class SimpleAsyncioServer(asyncio.Protocol):
+    def __init__(self, callback, *args, **kwargs):
         self.transport: Optional[TCPTransport] = None
-        super(HardcodedHTTPResponse, self).__init__(*args, **kwargs)
+        self.callback = callback
+        super(SimpleAsyncioServer, self).__init__(*args, **kwargs)
 
     def connection_made(self, transport: TCPTransport):
         self.transport = transport
 
     def data_received(self, data: bytes):
-        await self.transport.write(b"HTTP/1.1 200 OK\r\nContent-Length:13\r\n\r\nHello, world!")
+        asyncio.create_task(self.callback(self.transport, data))
+
+
+async def process_request(transport, data: bytes):
+    transport.write(
+        b"HTTP/1.1 200 OK\r\nContent-Length:13\r\n\r\nHello, world!"
+    )
 
 
 def run_main(host, port):
@@ -23,17 +31,23 @@ def run_main(host, port):
 
 async def main(host, port):
     loop = asyncio.get_running_loop()
-    server = await loop.create_server(HardcodedHTTPResponse, host, port, reuse_port=True)
+    server = await loop.create_server(
+        lambda: SimpleAsyncioServer(process_request),
+        host, port,
+        reuse_port=True
+    )
+
     await server.serve_forever()
 
 
 children = []
 uvloop.install()
 
-for i in range(cpu_count() - 1):
-    process = Process(target=run_main, args=('127.0.0.1', 9092,))
-    process.start()
-    children.append(process)
+if __name__ == '__main__':
+    for i in range(cpu_count() - 1):
+        process = Process(target=run_main, args=('127.0.0.1', 9092,))
+        process.start()
+        children.append(process)
 
 try:
     run_main('127.0.0.1', 9092)
