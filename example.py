bfe2d7eaa3974155e4d22d0489b48da81a2e3b64
floordiv <pavloromanenko25@gmail.com>
2021-12-01 01:17:55 +0200

    example: completely updated to be more explicit, wriitten small middleware that demonstrates how does it look like (also new Request.ctx is shown here)

--- example.py
+++ example.py
@@ -1,34 +1,52 @@
-from rush import webserver, entities
-from rush.dispatcher.default import SimpleAsyncDispatcher, Route
+from typing import Awaitable
 
-dp = SimpleAsyncDispatcher()
+from rush import webserver
+from rush.entities import Request, Response
+from rush.middlewares.base import BaseMiddleware
+from rush.dispatcher.default import AsyncDispatcher, Route
+
+dp = AsyncDispatcher()
 app = webserver.WebServer()
 
 
+class MyMiddleware(BaseMiddleware):
+    async def process(self, handler: Awaitable, request: Request) -> Response:
+        request.ctx.hello = 'hello, world!'
+        response = await handler
+        response.add_body(f'\nHandler said: {request.ctx["from_handler"]}')
+
+        return response
+
+
 @dp.get('/')
-async def deco_handler(request: entities.Request) -> None:
-    return request.response(
+async def deco_handler(request: Request, response: Response) -> Response:
+    return response(
         code=200,
         body=b'Hello, world!'
     )
 
 
 @dp.get('/get-request-fields')
-async def awaiting_demo(request: entities.Request) -> None:
-    method: bytes = request.method
-    path: bytes = request.path
-    headers: entities.CaseInsensitiveDict = request.headers
-    body: bytes = request.body
-    
-    return request.response(
+async def awaiting_demo(request: Request, response: Response) -> Response:
+    return response(
         code=200,
-        body=b'great job, buddy'
+        body=b'method: %s\npath: %s\nbody: %s' % (
+            request.method, request.path, request.body
+        )
     )
 
 
-async def echo_req_body_handler(request: entities.Request) -> None:
+async def middleware_example(request: Request, response: Response) -> Response:
+    request.ctx.from_handler = 'with love'
+
+    return response(
+        body=f'Middleware said: {request.ctx["hello"]}'
+    )
+
+
+async def echo_req_body_handler(request: Request, response: Response) -> Response:
     if 'easter' in request.headers:
-        return request.response(
+        return response(
             code=201,
             body=b'wow, you found an easter egg!'
         )
@@ -36,14 +54,18 @@ async def echo_req_body_handler(request: entities.Request) -> None:
     # it shouldn't be returned cause only one call of 
     # request.response() responses. Used returns for
     # more defined & usual behaviour
-    request.response(
+    return response(
         code=200,
         body=request.body
     )
 
 
 dp.add_routes([
-    Route(echo_req_body_handler, '/echo', 'GET')
+    Route(echo_req_body_handler, '/echo', 'GET'),
+    Route(middleware_example, '/middlewares', 'GET',
+          middlewares=[
+              MyMiddleware()
+          ])
 ])
 
 app.run(dp)
