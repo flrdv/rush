1764c9d2b160dae61a4a35dd1062b8010ba67439
floordiv <pavloromanenko25@gmail.com>
2021-12-01 01:19:39 +0200

    rush.dispatcher.default: added middlewares support. Updated typehints, removed prefix Simple in SimpleAsyncDispatcher, added msg for HTTPNotFoundError exception if there is no registered handlers for request path

--- rush/dispatcher/default.py
+++ rush/dispatcher/default.py
@@ -1,12 +1,14 @@
+from functools import reduce
 from asyncio import iscoroutinefunction
-from typing import (Dict, Any, Callable, Awaitable, Union, Iterable)
+from typing import (Dict, Callable, Awaitable, Union, Iterable, List, Optional)
 
 from .. import exceptions
-from .base import Dispatcher
-from ..entities import Request
-from ..utils.httpmethods import ALL_METHODS
-from ..typehints import RoutePath, Coroutine
+from .base import BaseDispatcher
+from ..entities import Request, Response
+from ..middlewares.base import BaseMiddleware
 from ..utils.stringutils import make_sure_bytes_or_none
+from ..typehints import RoutePath, AsyncFunction, HTTPMethod
+from ..utils.httputils import HTTP_METHODS, render_http_response
 
 
 class Handler:
@@ -16,15 +18,29 @@ class Handler:
     """
 
     def __init__(self,
-                 handler: Callable[[Request], Awaitable],
+                 handler: Callable[[Request, Response], Awaitable],
                  path: RoutePath,
                  methods: Iterable[bytes],
-                 any_path: bool
+                 any_path: bool,
+                 middlewares: List[BaseMiddleware]
                  ):
         self.handler = handler
         self.path = path
         self.methods = methods
         self.any_path = any_path
+        self.middlewares = middlewares or []
+
+    def get_performed_middleware(self,
+                                 request: Request,
+                                 response: Response) -> Awaitable:
+        return reduce(
+            lambda prev, next_: next_.process(prev, request),
+            # all the middlewares `process()` method requires handler
+            # but endpoint handler doesn't. So we just put endpoint handler
+            # as the first element in the list of middlewares so we don't need
+            # to pass any arguments to it. Ez solution, but ugly for linters
+            [self.handler(request, response)] + self.middlewares  # noqa
+        )
 
 
 class Route:
@@ -34,9 +50,10 @@ class Route:
     """
 
     def __init__(self,
-                 handler: Coroutine,
+                 handler: AsyncFunction,
                  path: RoutePath,
-                 method_or_methods: Union[str, bytes, Iterable] = ALL_METHODS
+                 method_or_methods: Union[str, bytes, Iterable] = HTTP_METHODS,
+                 middlewares: Optional[List[BaseMiddleware]] = None
                  ):
         self.handler = handler
         self.path = path if isinstance(path, bytes) else path.encode()
@@ -45,41 +62,60 @@ class Route:
             method_or_methods = {method_or_methods}
 
         self.methods = method_or_methods
+        self.middlewares = middlewares or []
 
 
-class SimpleAsyncDispatcher(Dispatcher):
+class AsyncDispatcher(BaseDispatcher):
     def __init__(self):
         self.usual_handlers: Dict[bytes, Handler] = {}
-        self.any_paths_handlers: Dict[Any[ALL_METHODS], Handler] = {
-            method: None for method in ALL_METHODS
+        self.any_paths_handlers: Dict[HTTPMethod, Handler] = {
+            method: None for method in HTTP_METHODS
         }
 
     async def process_request(self,
-                              request: Request
+                              request: Request,
+                              response: Response,
+                              http_send: Callable[[bytes], None]
                               ) -> None:
-        # print('processing request with path:', request.path, 'and method:', request.method)
-        # print(self.usual_handlers)
         if request.path not in self.usual_handlers:
             handler = self.any_paths_handlers[request.method]
 
             if handler is None:
-                raise exceptions.HTTPNotFound(request)
+                raise exceptions.HTTPNotFound(request, msg='no-registered-routers')
         else:
             handler = self.usual_handlers[request.path]
 
-        await handler.handler(request)
-        await request.wipe()
+        if handler.middlewares:
+            result = await handler.get_performed_middleware(request, response)
+        else:
+            result = await handler.handler(request, response)
+
+        http_send(
+            render_http_response(
+                protocol=b'1.1',
+                code=result.code,
+                status_code=result.status,
+                headers=result.headers if result.headers is not None else result.default_headers,
+                body=result.body,
+                # TODO: this shouldn't be always True, will be updated after chunked transfer
+                #       will be implemented
+                count_content_length=True
+            )
+        )
+
+        request.wipe()
 
     def route(self,
               path: RoutePath,
               method: Union[str, bytes, None] = None,
-              methods: Iterable[bytes] = ALL_METHODS
+              methods: Iterable[HTTPMethod] = HTTP_METHODS,
+              middlewares: Optional[List[BaseMiddleware]] = None
               ):
 
         if method is not None:
             methods = {make_sure_bytes_or_none(method.upper())}
 
-        def deco(coro: Callable[[Request], Awaitable]):
+        def deco(coro: Callable[[Request, Response], Awaitable]):
             if not methods:
                 raise exceptions.NoMethodsProvided(str(coro))
 
@@ -90,15 +126,15 @@ class SimpleAsyncDispatcher(Dispatcher):
                 handler=coro,
                 path=make_sure_bytes_or_none(path),
                 methods=set(methods),
-                any_path=path is None
+                any_path=path is None,
+                middlewares=middlewares or []
             ))
 
             return coro
 
         return deco
 
-    def get(self,
-            path: RoutePath):
+    def get(self, path: RoutePath):
         return self.route(path, 'GET')
 
     def post(self, path: RoutePath):
@@ -134,7 +170,8 @@ class SimpleAsyncDispatcher(Dispatcher):
             handler=route.handler,
             path=route.path,
             methods=route.methods,
-            any_path=not route.path
+            any_path=not route.path,
+            middlewares=route.middlewares
         ))
 
     def _put_handler(self, handler: Handler) -> None:
