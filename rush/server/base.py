f17f002b3f02fcc70157dc9ef37d2bb3f4b1c1c7
floordiv <pavloromanenko25@gmail.com>
2021-11-30 22:42:28 +0200

    rush.server.base: made methods abstract, added abstract method `stop()`

--- rush/server/base.py
+++ rush/server/base.py
@@ -2,9 +2,16 @@ import abc
 
 
 class HTTPServer(abc.ABC):
-    def poll(self) -> None:
+    @abc.abstractmethod
+    async def poll(self) -> None:
         """
         Blocking function that runs server infinity, but may be interrupted
         by exception that should be caught by webserver, processed, and
         continue polling by calling this function again
+        """
+
+    @abc.abstractmethod
+    def stop(self):
+        """
+        Stops the server in correct way
         """
