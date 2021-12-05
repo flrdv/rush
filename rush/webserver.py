c76e18c3a022b4a83c5963380867124e6cb7eaa3
floordiv <pavloromanenko25@gmail.com>
2021-12-05 04:22:23 +0200

    rush.webserver: removed initializing logger by my own, now logger is setting in settings (TODO: fix typehint for any logger); made code more clear: removed setting processes count directly to settings object (moved this feature to a dedicated function), the same is made with forking; added detecting running under windows (and disabling forks for it by default); made `is_parent()` and `kill_children()` methods private; due to new implementation of `_is_parent()`, removed `parent_pid` attribute

--- rush/webserver.py
+++ rush/webserver.py
@@ -2,16 +2,15 @@ import os
 import socket
 import logging
 import asyncio
+import platform
 import multiprocessing
 from signal import SIGKILL
 from traceback import format_exc
 from dataclasses import dataclass, field
-from typing import List, Dict, Type, Union, Optional
+from typing import List, Type, Union, Optional
 
-from . import exceptions
 from .utils import sockutils
 from .server.base import HTTPServer
-from .typehints import AsyncFunction
 from .entities import CaseInsensitiveDict
 from .dispatcher.base import BaseDispatcher
 from .server.aiohttpserver import AioHTTPServer
@@ -19,9 +18,14 @@ from .storage import (base as storage_base,
                       fd_sendfile as storage_fd_sendfile)
 
 
+logging.basicConfig(
+    format='[%(asctime)s] [%(levelname)s] %(name)s: %(message)s'
+)
+
+
 @dataclass
 class Settings:
-    host: str = field(default='0.0.0.0')
+    host: str = field(default='127.0.0.1')
     port: int = field(default=9090)
     max_bind_retries: Optional[int] = field(default=None)
     bind_retries_timeout: Union[int, float] = field(default=3)
@@ -35,10 +39,7 @@ class Settings:
         )
     )
 
-    logging_level: int = field(default=logging.DEBUG)
-    logging_format: str = field(default='[%(asctime)s] [%(levelname)s] %(name)s: %(message)s')
-    logs_dir: str = field(default='logs')
-    logs_file: str = field(default='webserver.log')
+    logger: logging.Logger = field(default_factory=logging.getLogger)
 
     storage: Type[storage_base.Storage] = field(default=storage_fd_sendfile.SimpleDevStorage)
     httpserver: Type[HTTPServer] = field(default=AioHTTPServer)
@@ -51,34 +52,16 @@ class WebServer:
     def __init__(self,
                  settings: Settings = Settings(),
                  ):
-        if not os.path.exists(settings.logs_dir):
-            os.mkdir(settings.logs_dir)
-
-        logs_file_path = os.path.join(
-            settings.logs_dir, settings.logs_file
-        )
-
-        logging.basicConfig(
-            level=settings.logging_level,
-            format=settings.logging_format,
-            handlers=[
-                logging.FileHandler(logs_file_path),
-                logging.StreamHandler()
-            ]
-        )
-        self.logger = logging.getLogger()
-        self.logger.info(f'writing logs to {logs_file_path}')
-
+        self.logger = settings.logger
         asyncio_logger = logging.getLogger('asyncio')
         asyncio_logger.disabled = not settings.asyncio_logging
         asyncio_logger.setLevel(settings.asyncio_logging_level)
 
         self.settings = settings
 
-        self.children: List[int] = []
-        self.parent_pid: int = os.getpid()
-
-        self.http_errors_handlers: Dict[Type[exceptions.HTTPError], AsyncFunction] = {}
+        # if children is None, current process isn't parent
+        # only parent process has a list of children
+        self._children: Optional[List[int]] = []
 
     def run(self, dp: BaseDispatcher):
         """
@@ -93,35 +76,58 @@ class WebServer:
             raise TypeError(f'{dp} object must be inherited from '
                             'rush.dispatcher.base.Dispatcher object!')
 
-        if self.settings.processes is None:
-            # if processes count is set to None, set the number
-            # to processor logical cores count
-            logical_cores = multiprocessing.cpu_count()
-            self.logger.info(f'setting processes count to {logical_cores}')
-            self.settings.processes = logical_cores
+        children_count = self._get_children_count(self.settings.processes)
 
-        self.logger.debug(f'forking {self.settings.processes} times')
+        if children_count != self.settings.processes:
+            self.logger.info(f'setting processes count to {children_count}')
 
-        for child_num in range(self.settings.processes - 1):
-            child_pid = os.fork()
+        self.logger.debug(f'forking {children_count} times')
+        self._children = self._do_forks(n=children_count)
 
-            if child_pid != 0:
-                self.children.append(child_pid)
-                self.logger.debug(f'started child process id={child_num} pid={child_pid}')
-            else:
-                break
-
-        if self.is_parent():
+        if self._children is not None:
             self.logger.info('children has been spawned')
 
         self._server_worker(dp)
+
+    @staticmethod
+    def _get_children_count(raw_count: Optional[int]) -> int:
+        """
+        Returns a count of children has to be spawned
+
+        Returns 0 if Windows (as reuseport isn't available under windows)
+        Returns raw_count if raw_count is bigger than 1
+        Returns 0 if raw_count is 1
+        Returns multiprocessing.cpu_count() - 1 if raw_count is None
+        """
+
+        if platform.system() == 'Windows':
+            return 0
+
+        if raw_count is None:
+            return multiprocessing.cpu_count() - 1
+
+        return raw_count - 1
+
+    def _do_forks(self, n: int) -> Optional[List[int]]:
+        spawned_children = []
+
+        for child_num in range(n - 1):
+            child_pid = os.fork()
+
+            if child_pid != 0:
+                spawned_children.append(child_pid)
+                self.logger.debug(f'started child process id={child_num} pid={child_pid}')
+            else:
+                return
+
+        return spawned_children
 
     def _server_worker(self, dp: BaseDispatcher):
         """
         Finally, we're in our brand-new process that belongs only to us, hohoho
         """
 
-        self.logger.disabled = not self.is_parent()
+        self.logger.disabled = not self._is_parent()
 
         sock = socket.socket()
         sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, True)
@@ -142,15 +148,15 @@ class WebServer:
                               f'retries_timeout={retries_timeout}, '
                               f'time_elapsed={round(bind_retries * retries_timeout, 2)}secs)')
 
-            if self.is_parent():
+            if self._is_parent():
                 self.logger.critical('the problem was caused in parent process, shutting down'
                                      'the server')
-                self.kill_children()
+                self._kill_children()
 
             sock.close()
             raise SystemExit(1)
 
-        if self.is_parent():
+        if self._is_parent():
             self.logger.info(f'successfully bound socket on {self.settings.host}:{self.settings.port}')
             self.logger.info('press CTRL-C to stop the server')
 
@@ -167,9 +173,9 @@ class WebServer:
                 loop = asyncio.new_event_loop()
                 loop.run_until_complete(http_server.poll())
             except (KeyboardInterrupt, SystemExit, EOFError):
-                if self.is_parent():
+                if self._is_parent():
                     self.logger.info('shutting down (aborted by user)...')
-                    self.kill_children()
+                    self._kill_children()
                     http_server.stop()
 
                     break
@@ -181,37 +187,46 @@ class WebServer:
                 self.logger.exception(f'an error occurred while running http server: {exc}\n'
                                       f'Detailed trace:\n{format_exc()}')
                 self.logger.warning('most of all, this is a bug; please, open an issue on '
-                                    'https://github.com/floordiv/rush/issues with detailed description '
-                                    'of the case, environment, and, if possible, a sample of code to '
+                                    'https://github.com/floordiv/rush/issues with detailed '
+                                    'description of the case, web-server version, traceback, '
+                                    'environment, and, if it\'s possible, a sample of code to '
                                     'reproduce an error')
                 self.logger.info('continuing the job')
 
-    def is_parent(self) -> bool:
+    def _is_parent(self) -> bool:
         """
         Returns True or False, depending on fact whether we're in parent process
         or not. Mainly used for internal purposes, but also can be used by user
         for some reason (really idk for what user should use this)
         """
 
-        return os.getpid() == self.parent_pid
+        return self._children is not None
 
-    def kill_children(self):
-        if self.is_parent():
+    def _kill_children(self):
+        if self._is_parent():
             self.logger.debug('killing children...')
 
-            for child in self.children:
-                os.kill(child, SIGKILL)
+            for child in self._children:
+                try:
+                    os.kill(child, SIGKILL)
+                except SystemError as syserr:
+                    self.logger.warning(f'failed to kill children pid={child}: {syserr}')
 
-            self.children.clear()
+            self._children.clear()
             self.logger.info('killed all the children')
 
     def stop(self):
         """
         Here should be implemented calling some events callbacks
         or something like that
-        and killing children
+        Currently the only thing we need is just killing all the children
+
+        Maybe, also should be good to close server here, but it should be closed
+        by itself because there is no way to implement this without using
+        shit code
         """
-        self.kill_children()
+
+        self._kill_children()
 
     def __del__(self):
         self.stop()
