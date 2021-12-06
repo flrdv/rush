f1899c4a1d7637e58641a31cd7a2a8ae71d76543
floordiv <pavloromanenko25@gmail.com>
2021-12-06 03:39:02 +0200

    rush.typehints: added protocol for logger object

--- rush/typehints.py
+++ rush/typehints.py
@@ -1,5 +1,5 @@
 import socket
-from typing import Callable, Awaitable, BinaryIO, Union, NewType
+from typing import Callable, Awaitable, BinaryIO, Union, NewType, Protocol
 
 AsyncFunction = Callable[..., Awaitable]
 Path = str
@@ -14,3 +14,23 @@ HTTPVersion = str
 Connection = socket.socket
 HttpResponseCallback = Callable
 Nothing = NewType('Nothing', None)
+
+
+class Logger(Protocol):
+    def debug(self, text: str) -> None:
+        ...
+
+    def info(self, text: str) -> None:
+        ...
+
+    def warning(self, text: str) -> None:
+        ...
+
+    def error(self, text: str) -> None:
+        ...
+
+    def critical(self, text: str) -> None:
+        ...
+
+    def exception(self, text: str) -> None:
+        ...
