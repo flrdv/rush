444e676bcff7bfb5d3cb53585b4990c791336ecb
floordiv <pavloromanenko25@gmail.com>
2021-12-06 04:59:04 +0200

    rush.middlewares.base: fixed invalid typehint for handler object: should be Awaitable, was AsyncFunction instead

--- rush/middlewares/base.py
+++ rush/middlewares/base.py
@@ -1,13 +1,13 @@
 import abc
+from typing import Awaitable
 
-from ..typehints import AsyncFunction
 from ..entities import Request, Response
 
 
 class BaseMiddleware(abc.ABC):
     @abc.abstractmethod
     async def process(self,
-                      handler: AsyncFunction,
+                      handler: Awaitable,
                       request: Request) -> Response:
         """
         A place where handler must be called. Note: handler is not always _endpoint_ handler,
